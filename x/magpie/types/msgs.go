920d91a2366a7540ce5ab52ae184cc7c553ed11e
Kwun Yeung <kwun@forbole.com>
2019-07-15 20:34:50 +0800

    create MsgCreateSession message

--- x/magpie/types/msgs.go
+++ x/magpie/types/msgs.go
@@ -182,3 +182,57 @@ func (msg MsgUnlike) GetSignBytes() []byte {
 func (msg MsgUnlike) GetSigners() []sdk.AccAddress {
 	return []sdk.AccAddress{msg.Liker}
 }
+
+// MsgCreateSession defines the MsgCreateSession message
+type MsgCreateSession struct {
+	Owner         sdk.AccAddress
+	Created       time.Time
+	Namespace     string
+	ExternalOwner sdk.AccAddress
+}
+
+// NewMsgCreateSession is the contructor of MsgUnlike
+func NewMsgCreateSession(created time.Time, owner sdk.AccAddress, namespace string, externalOwner sdk.AccAddress) MsgCreateSession {
+	return MsgCreateSession{
+		Created:       created,
+		Owner:         owner,
+		Namespace:     namespace,
+		ExternalOwner: externalOwner,
+	}
+}
+
+// Route should return the name of the module
+func (msg MsgCreateSession) Route() string { return RouterKey }
+
+// Type should return the action
+func (msg MsgCreateSession) Type() string { return "create_session" }
+
+// ValidateBasic runs stateless checks on the message
+func (msg MsgCreateSession) ValidateBasic() sdk.Error {
+	if msg.Owner.Empty() {
+		return sdk.ErrInvalidAddress(msg.Owner.String())
+	}
+
+	if msg.Created.IsZero() {
+		return sdk.ErrUnknownRequest("Session created time cannot be empty")
+	}
+
+	if len(msg.Namespace) == 0 {
+		return sdk.ErrUnknownRequest("Session namespace cannot be empty")
+	}
+
+	if msg.ExternalOwner.Empty() {
+		return sdk.ErrUnknownRequest("Session external owner cannot be empty")
+	}
+	return nil
+}
+
+// GetSignBytes encodes the message for signing
+func (msg MsgCreateSession) GetSignBytes() []byte {
+	return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(msg))
+}
+
+// GetSigners defines whose signature is required
+func (msg MsgCreateSession) GetSigners() []sdk.AccAddress {
+	return []sdk.AccAddress{msg.Owner}
+}
