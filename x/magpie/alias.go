ef7369aae2dff1af486b4d6a17590a585332dea8
Kwun Yeung <kwun@forbole.com>
2019-07-15 20:32:30 +0800

    add session alias

--- x/magpie/alias.go
+++ x/magpie/alias.go
@@ -15,20 +15,23 @@ var (
 	NewMsgEditPost   = types.NewMsgEditPost
 	NewMsgLike       = types.NewMsgLike
 	NewMsgUnlike     = types.NewMsgUnlike
+	NewMsgSession    = types.NewMsgCreateSession
 	NewLike          = types.NewLike
 	NewPost          = types.NewPost
+	NewSession       = types.NewSession
 	ModuleCdc        = types.ModuleCdc
 	RegisterCodec    = types.RegisterCodec
 )
 
 type (
-	MsgCreatePost   = types.MsgCreatePost
-	MsgEditPost     = types.MsgEditPost
-	MsgLike         = types.MsgLike
-	MsgUnlike       = types.MsgUnlike
-	Post            = types.Post
-	Like            = types.Like
-	QueryResPost    = types.QueryResPost
-	QueryResLike    = types.QueryResLike
-	QueryResSession = types.QueryResSession
+	MsgCreatePost    = types.MsgCreatePost
+	MsgEditPost      = types.MsgEditPost
+	MsgLike          = types.MsgLike
+	MsgUnlike        = types.MsgUnlike
+	MsgCreateSession = types.MsgCreateSession
+	Post             = types.Post
+	Like             = types.Like
+	QueryResPost     = types.QueryResPost
+	QueryResLike     = types.QueryResLike
+	QueryResSession  = types.QueryResSession
 )
