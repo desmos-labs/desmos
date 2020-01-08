0d6917f348d68818fabedeb481f7374fe501c7c2
Bragaz <Merda10#>
2020-01-08 12:09:33 +0100

    fixed tests errors

--- x/posts/internal/keeper/keeper_test.go
+++ x/posts/internal/keeper/keeper_test.go
@@ -49,7 +49,6 @@ func TestKeeper_SavePost(t *testing.T) {
 	tests := []struct {
 		name                 string
 		existingPosts        types.Posts
-		lastPostID           types.PostID
 		newPost              types.Post
 		expParentCommentsIDs types.PostIDs
 		expLastID            types.PostID
@@ -57,52 +56,26 @@ func TestKeeper_SavePost(t *testing.T) {
 		{
 			name: "Post with ID already present",
 			existingPosts: types.Posts{
-				types.NewPost(types.PostID(1), types.PostID(0), "Post", false, "", 0, testPostOwner),
+				types.NewPost(types.PostID(1), types.PostID(0), "Post", false, "desmos", map[string]string{}, 0, testPostOwner),
 			},
-			lastPostID:           types.PostID(1),
-			newPost:              types.NewPost(types.PostID(1), types.PostID(0), "New post", false, "", 0, testPostOwner),
+			newPost:              types.NewPost(types.PostID(1), types.PostID(0), "New post", false, "desmos", map[string]string{}, 0, testPostOwner),
 			expParentCommentsIDs: []types.PostID{},
-			expLastID:            types.PostID(1),
 		},
 		{
 			name: "Post which ID is not already present",
 			existingPosts: types.Posts{
-				types.NewPost(types.PostID(1), types.PostID(0), "Post", false, "", 0, testPostOwner),
+				types.NewPost(types.PostID(1), types.PostID(0), "Post", false, "desmos", map[string]string{}, 0, testPostOwner),
 			},
-			lastPostID:           types.PostID(1),
-			newPost:              types.NewPost(types.PostID(15), types.PostID(0), "New post", false, "", 0, testPostOwner),
+			newPost:              types.NewPost(types.PostID(15), types.PostID(0), "New post", false, "desmos", map[string]string{}, 0, testPostOwner),
 			expParentCommentsIDs: []types.PostID{},
-			expLastID:            types.PostID(15),
 		},
 		{
 			name: "Post with valid parent ID",
 			existingPosts: []types.Post{
-				types.NewPost(types.PostID(1), types.PostID(0), "Parent", false, "", 0, testPostOwner),
+				types.NewPost(types.PostID(1), types.PostID(0), "Parent", false, "desmos", map[string]string{}, 0, testPostOwner),
 			},
-			lastPostID:           types.PostID(1),
-			newPost:              types.NewPost(types.PostID(15), types.PostID(1), "Comment", false, "", 0, testPostOwner),
+			newPost:              types.NewPost(types.PostID(15), types.PostID(1), "Comment", false, "desmos", map[string]string{}, 0, testPostOwner),
 			expParentCommentsIDs: []types.PostID{types.PostID(15)},
-			expLastID:            types.PostID(15),
-		},
-		{
-			name: "Post with ID greater ID than Last ID stored",
-			existingPosts: types.Posts{
-				types.NewPost(types.PostID(4), types.PostID(0), "Post lesser", false, "", 0, testPostOwner),
-			},
-			lastPostID:           types.PostID(4),
-			newPost:              types.NewPost(types.PostID(5), types.PostID(0), "New post greater", false, "", 0, testPostOwner),
-			expParentCommentsIDs: []types.PostID{},
-			expLastID:            types.PostID(5),
-		},
-		{
-			name: "Post with ID lesser ID than Last ID stored",
-			existingPosts: types.Posts{
-				types.NewPost(types.PostID(4), types.PostID(0), "Post ID greater", false, "", 0, testPostOwner),
-			},
-			lastPostID:           types.PostID(4),
-			newPost:              types.NewPost(types.PostID(3), types.PostID(0), "New post ID lesser", false, "", 0, testPostOwner),
-			expParentCommentsIDs: []types.PostID{},
-			expLastID:            types.PostID(4),
 		},
 	}
 
@@ -114,7 +87,6 @@ func TestKeeper_SavePost(t *testing.T) {
 			store := ctx.KVStore(k.StoreKey)
 			for _, p := range test.existingPosts {
 				store.Set([]byte(types.PostStorePrefix+p.PostID.String()), k.Cdc.MustMarshalBinaryBare(p))
-				store.Set([]byte(types.LastPostIDStoreKey), k.Cdc.MustMarshalBinaryBare(test.lastPostID))
 			}
 
 			// Save the post
@@ -123,12 +95,12 @@ func TestKeeper_SavePost(t *testing.T) {
 			// Check the stored post
 			var expected types.Post
 			k.Cdc.MustUnmarshalBinaryBare(store.Get([]byte(types.PostStorePrefix+test.newPost.PostID.String())), &expected)
-			assert.Equal(t, test.newPost, expected)
+			assert.True(t, expected.Equals(test.newPost))
 
 			// Check the latest post id
 			var lastPostID types.PostID
 			k.Cdc.MustUnmarshalBinaryBare(store.Get([]byte(types.LastPostIDStoreKey)), &lastPostID)
-			assert.Equal(t, test.expLastID, lastPostID)
+			assert.Equal(t, test.newPost.PostID, lastPostID)
 
 			// Check the parent comments
 			var parentCommentsIDs []types.PostID
@@ -154,7 +126,7 @@ func TestKeeper_GetPost(t *testing.T) {
 			name:       "Existing post is found properly",
 			ID:         types.PostID(45),
 			postExists: true,
-			expected:   types.NewPost(types.PostID(45), types.PostID(0), "Post", false, "", 0, testPostOwner),
+			expected:   types.NewPost(types.PostID(45), types.PostID(0), "Post", false, "desmos", map[string]string{}, 0, testPostOwner),
 		},
 	}
 
@@ -171,7 +143,7 @@ func TestKeeper_GetPost(t *testing.T) {
 			expected, found := k.GetPost(ctx, test.ID)
 			assert.Equal(t, test.postExists, found)
 			if test.postExists {
-				assert.Equal(t, test.expected, expected)
+				assert.True(t, expected.Equals(test.expected))
 			}
 		})
 	}
@@ -192,11 +164,11 @@ func TestKeeper_GetPostChildrenIDs(t *testing.T) {
 		{
 			name: "Non empty children list is returned properly",
 			storedPosts: types.Posts{
-				types.NewPost(types.PostID(10), types.PostID(0), "Original post", false, "", 10, testPost.Owner),
-				types.NewPost(types.PostID(55), types.PostID(10), "First commit", false, "", 10, testPost.Owner),
-				types.NewPost(types.PostID(78), types.PostID(10), "Other commit", false, "", 10, testPost.Owner),
-				types.NewPost(types.PostID(11), types.PostID(0), "Second post", false, "", 10, testPost.Owner),
-				types.NewPost(types.PostID(104), types.PostID(11), "Comment to second post", false, "", 10, testPost.Owner),
+				types.NewPost(types.PostID(10), types.PostID(0), "Original post", false, "desmos", map[string]string{}, 10, testPostOwner),
+				types.NewPost(types.PostID(55), types.PostID(10), "First commit", false, "desmos", map[string]string{}, 10, testPostOwner),
+				types.NewPost(types.PostID(78), types.PostID(10), "Other commit", false, "desmos", map[string]string{}, 10, testPostOwner),
+				types.NewPost(types.PostID(11), types.PostID(0), "Second post", false, "desmos", map[string]string{}, 10, testPostOwner),
+				types.NewPost(types.PostID(104), types.PostID(11), "Comment to second post", false, "desmos", map[string]string{}, 10, testPostOwner),
 			},
 			postID:         types.PostID(10),
 			expChildrenIDs: types.PostIDs{types.PostID(55), types.PostID(78)},
@@ -234,8 +206,8 @@ func TestKeeper_GetPosts(t *testing.T) {
 		{
 			name: "Existing list is returned properly",
 			posts: types.Posts{
-				types.NewPost(types.PostID(13), types.PostID(0), "", false, "", 0, testPostOwner),
-				types.NewPost(types.PostID(76), types.PostID(0), "", false, "", 0, testPostOwner),
+				types.NewPost(types.PostID(13), types.PostID(0), "", false, "desmos", map[string]string{}, 0, testPostOwner),
+				types.NewPost(types.PostID(76), types.PostID(0), "", false, "desmos", map[string]string{}, 0, testPostOwner),
 			},
 		},
 	}
@@ -251,56 +223,52 @@ func TestKeeper_GetPosts(t *testing.T) {
 			}
 
 			posts := k.GetPosts(ctx)
-			assert.Len(t, posts, len(test.posts))
-
-			for _, p := range test.posts {
-				assert.Contains(t, posts, p)
-			}
+			assert.True(t, test.posts.Equals(posts))
 		})
 	}
 }
 
 // -------------
-// --- Likes
+// --- Reactions
 // -------------
 
-func TestKeeper_SaveLike(t *testing.T) {
+func TestKeeper_SaveReaction(t *testing.T) {
 	liker, _ := sdk.AccAddressFromBech32("cosmos1s3nh6tafl4amaxkke9kdejhp09lk93g9ev39r4")
 	otherLiker, _ := sdk.AccAddressFromBech32("cosmos15lt0mflt6j9a9auj7yl3p20xec4xvljge0zhae")
 
 	tests := []struct {
 		name           string
-		storedLikes    types.Likes
+		storedLikes    types.Reactions
 		postID         types.PostID
-		like           types.Like
+		like           types.Reaction
 		error          sdk.Error
-		expectedStored types.Likes
+		expectedStored types.Reactions
 	}{
 		{
-			name:           "Like from same liker already present returns error",
-			storedLikes:    types.Likes{types.NewLike(10, liker)},
+			name:           "Reaction from same user already present returns expError",
+			storedLikes:    types.Reactions{types.NewReaction("like", 10, liker)},
 			postID:         types.PostID(10),
-			like:           types.NewLike(50, liker),
-			error:          sdk.ErrUnknownRequest("cosmos1s3nh6tafl4amaxkke9kdejhp09lk93g9ev39r4 has already liked the post with id 10"),
-			expectedStored: types.Likes{types.NewLike(10, liker)},
+			like:           types.NewReaction("like", 50, liker),
+			error:          sdk.ErrUnknownRequest("cosmos1s3nh6tafl4amaxkke9kdejhp09lk93g9ev39r4 has already reacted with like to the post with id 10"),
+			expectedStored: types.Reactions{types.NewReaction("like", 10, liker)},
 		},
 		{
 			name:           "First liker is stored properly",
-			storedLikes:    types.Likes{},
+			storedLikes:    types.Reactions{},
 			postID:         types.PostID(15),
-			like:           types.NewLike(15, liker),
+			like:           types.NewReaction("like", 15, liker),
 			error:          nil,
-			expectedStored: types.Likes{types.NewLike(15, liker)},
+			expectedStored: types.Reactions{types.NewReaction("like", 15, liker)},
 		},
 		{
 			name:        "Second liker is stored properly",
-			storedLikes: types.Likes{types.NewLike(10, liker)},
+			storedLikes: types.Reactions{types.NewReaction("like", 10, liker)},
 			postID:      types.PostID(87),
-			like:        types.NewLike(1, otherLiker),
+			like:        types.NewReaction("like", 1, otherLiker),
 			error:       nil,
-			expectedStored: types.Likes{
-				types.NewLike(10, liker),
-				types.NewLike(1, otherLiker),
+			expectedStored: types.Reactions{
+				types.NewReaction("like", 10, liker),
+				types.NewReaction("like", 1, otherLiker),
 			},
 		},
 	}
@@ -312,45 +280,57 @@ func TestKeeper_SaveLike(t *testing.T) {
 
 			store := ctx.KVStore(k.StoreKey)
 			if len(test.storedLikes) != 0 {
-				store.Set([]byte(types.LikesStorePrefix+test.postID.String()), k.Cdc.MustMarshalBinaryBare(&test.storedLikes))
+				store.Set([]byte(types.PostReactionsStorePrefix+test.postID.String()), k.Cdc.MustMarshalBinaryBare(&test.storedLikes))
 			}
 
-			err := k.SaveLike(ctx, test.postID, test.like)
+			err := k.SaveReaction(ctx, test.postID, test.like)
 			assert.Equal(t, test.error, err)
 
-			var stored types.Likes
-			k.Cdc.MustUnmarshalBinaryBare(store.Get([]byte(types.LikesStorePrefix+test.postID.String())), &stored)
+			var stored types.Reactions
+			k.Cdc.MustUnmarshalBinaryBare(store.Get([]byte(types.PostReactionsStorePrefix+test.postID.String())), &stored)
 			assert.Equal(t, test.expectedStored, stored)
 		})
 	}
 }
 
-func TestKeeper_RemoveLike(t *testing.T) {
+func TestKeeper_RemoveReaction(t *testing.T) {
 	liker, _ := sdk.AccAddressFromBech32("cosmos1s3nh6tafl4amaxkke9kdejhp09lk93g9ev39r4")
 
 	tests := []struct {
 		name           string
-		storedLikes    types.Likes
+		storedLikes    types.Reactions
 		postID         types.PostID
 		liker          sdk.AccAddress
+		value          string
 		error          sdk.Error
-		expectedStored types.Likes
+		expectedStored types.Reactions
 	}{
 		{
-			name:           "Like from same liker is removed properly",
-			storedLikes:    types.Likes{types.NewLike(10, liker)},
+			name:           "Reaction from same liker is removed properly",
+			storedLikes:    types.Reactions{types.NewReaction("like", 10, liker)},
 			postID:         types.PostID(10),
 			liker:          liker,
+			value:          "like",
 			error:          nil,
-			expectedStored: types.Likes{},
+			expectedStored: types.Reactions{},
 		},
 		{
-			name:           "Non existing like returned error",
-			storedLikes:    types.Likes{},
+			name:           "Non existing like returned expError - Owner",
+			storedLikes:    types.Reactions{},
 			postID:         types.PostID(15),
 			liker:          liker,
-			error:          sdk.ErrUnauthorized("Cannot unlike a post without liking it"),
-			expectedStored: types.Likes{},
+			value:          "like",
+			error:          sdk.ErrUnauthorized("Cannot remove the reaction with value like from user cosmos1s3nh6tafl4amaxkke9kdejhp09lk93g9ev39r4 as it does not exist"),
+			expectedStored: types.Reactions{},
+		},
+		{
+			name:           "Non existing like returned expError - Value",
+			storedLikes:    types.Reactions{types.NewReaction("like", 10, liker)},
+			postID:         types.PostID(15),
+			liker:          liker,
+			value:          "reaction",
+			error:          sdk.ErrUnauthorized("Cannot remove the reaction with value reaction from user cosmos1s3nh6tafl4amaxkke9kdejhp09lk93g9ev39r4 as it does not exist"),
+			expectedStored: types.Reactions{types.NewReaction("like", 10, liker)},
 		},
 	}
 
@@ -361,14 +341,14 @@ func TestKeeper_RemoveLike(t *testing.T) {
 
 			store := ctx.KVStore(k.StoreKey)
 			if len(test.storedLikes) != 0 {
-				store.Set([]byte(types.LikesStorePrefix+test.postID.String()), k.Cdc.MustMarshalBinaryBare(&test.storedLikes))
+				store.Set([]byte(types.PostReactionsStorePrefix+test.postID.String()), k.Cdc.MustMarshalBinaryBare(&test.storedLikes))
 			}
 
-			err := k.RemoveLike(ctx, test.postID, test.liker)
+			err := k.RemoveReaction(ctx, test.postID, test.liker, test.value)
 			assert.Equal(t, test.error, err)
 
-			var stored types.Likes
-			k.Cdc.MustUnmarshalBinaryBare(store.Get([]byte(types.LikesStorePrefix+test.postID.String())), &stored)
+			var stored types.Reactions
+			k.Cdc.MustUnmarshalBinaryBare(store.Get([]byte(types.PostReactionsStorePrefix+test.postID.String())), &stored)
 
 			assert.Len(t, stored, len(test.expectedStored))
 			for index, like := range test.expectedStored {
@@ -384,19 +364,19 @@ func TestKeeper_GetPostLikes(t *testing.T) {
 
 	tests := []struct {
 		name   string
-		likes  types.Likes
+		likes  types.Reactions
 		postID types.PostID
 	}{
 		{
 			name:   "Empty list are returned properly",
-			likes:  types.Likes{},
+			likes:  types.Reactions{},
 			postID: types.PostID(10),
 		},
 		{
 			name: "Valid list of likes is returned properly",
-			likes: types.Likes{
-				types.NewLike(11, otherLiker),
-				types.NewLike(10, liker),
+			likes: types.Reactions{
+				types.NewReaction("like", 11, otherLiker),
+				types.NewReaction("like", 10, liker),
 			},
 		},
 	}
@@ -407,10 +387,10 @@ func TestKeeper_GetPostLikes(t *testing.T) {
 			ctx, k := SetupTestInput()
 
 			for _, l := range test.likes {
-				_ = k.SaveLike(ctx, test.postID, l)
+				_ = k.SaveReaction(ctx, test.postID, l)
 			}
 
-			stored := k.GetPostLikes(ctx, test.postID)
+			stored := k.GetPostReactions(ctx, test.postID)
 
 			assert.Len(t, stored, len(test.likes))
 			for _, l := range test.likes {
@@ -426,21 +406,21 @@ func TestKeeper_GetLikes(t *testing.T) {
 
 	tests := []struct {
 		name  string
-		likes map[types.PostID]types.Likes
+		likes map[types.PostID]types.Reactions
 	}{
 		{
 			name:  "Empty likes data are returned correctly",
-			likes: map[types.PostID]types.Likes{},
+			likes: map[types.PostID]types.Reactions{},
 		},
 		{
 			name: "Non empty likes data are returned correcly",
-			likes: map[types.PostID]types.Likes{
+			likes: map[types.PostID]types.Reactions{
 				types.PostID(5): {
-					types.NewLike(10, liker1),
-					types.NewLike(50, liker2),
+					types.NewReaction("like", 10, liker1),
+					types.NewReaction("like", 50, liker2),
 				},
 				types.PostID(10): {
-					types.NewLike(5, liker1),
+					types.NewReaction("like", 5, liker1),
 				},
 			},
 		},
@@ -452,10 +432,10 @@ func TestKeeper_GetLikes(t *testing.T) {
 			ctx, k := SetupTestInput()
 			store := ctx.KVStore(k.StoreKey)
 			for postID, likes := range test.likes {
-				store.Set([]byte(types.LikesStorePrefix+postID.String()), k.Cdc.MustMarshalBinaryBare(likes))
+				store.Set([]byte(types.PostReactionsStorePrefix+postID.String()), k.Cdc.MustMarshalBinaryBare(likes))
 			}
 
-			likesData := k.GetLikes(ctx)
+			likesData := k.GetReactions(ctx)
 			assert.Equal(t, test.likes, likesData)
 		})
 	}
