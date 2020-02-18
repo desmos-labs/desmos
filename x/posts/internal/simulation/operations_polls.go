02e7a13730060b64185acf45e946986b3619bccc
riccardo.montagnin <riccardo.montagnin@gmail.com>
2020-02-18 12:24:44 +0100

    Removed nondeterministic tests again

--- x/posts/internal/simulation/operations_polls.go
+++ x/posts/internal/simulation/operations_polls.go
@@ -57,7 +57,7 @@ func sendMsgAnswerPoll(
 	tx := helpers.GenTx(
 		[]sdk.Msg{msg},
 		fees,
-		2000000,
+		DefaultGasValue,
 		chainID,
 		[]uint64{account.GetAccountNumber()},
 		[]uint64{account.GetSequence()},
