4d15cae1387899755b993e1756150f0888168039
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2020-03-26 15:12:16 +0100

    Update x/posts/legacy/v0.2.0/types.go

--- x/posts/legacy/v0.2.0/types.go
+++ x/posts/legacy/v0.2.0/types.go
@@ -67,6 +67,6 @@ func (p Post) ContentsEquals(other Post) bool {
 
 // Reaction is a struct of a user reaction to a post
 type Reaction struct {
-	Owner sdk.AccAddress `json:"owner"` // Creator that has created the reaction
+	Owner sdk.AccAddress `json:"owner"` // User that has created the reaction
 	Value string         `json:"value"` // Value of the reaction
 }
