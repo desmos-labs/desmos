d668258e04bec4fd2ad0697e31774e81291f09f7
bragaz <leo.braga95@gmail.com>
2020-04-23 10:44:39 +0200

    added profile module invariants

--- x/profile/internal/keeper/invariants.go
+++ x/profile/internal/keeper/invariants.go
@@ -1,22 +1,23 @@
 package keeper
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/desmos-labs/desmos/x/profile/internal/types"
 )
 
 // RegisterInvariants registers all posts invariants
 func RegisterInvariants(ir sdk.InvariantRegistry, keeper Keeper) {
-	ir.RegisterRoute(types.ModuleName, "invariant-profile",
-		InvariantProfile(keeper))
+	ir.RegisterRoute(types.ModuleName, "valid-profile",
+		NonEmptyProfileCreatorOrMonikerInvariant(keeper))
 }
 
 //TODO not sure if this should be used or can be deleted
-
 // AllInvariants runs all invariants of the module
 func AllInvariants(k Keeper) sdk.Invariant {
 	return func(ctx sdk.Context) (string, bool) {
-		if res, stop := InvariantProfile(k)(ctx); stop {
+		if res, stop := NonEmptyProfileCreatorOrMonikerInvariant(k)(ctx); stop {
 			return res, stop
 		}
 
@@ -24,8 +25,29 @@ func AllInvariants(k Keeper) sdk.Invariant {
 	}
 }
 
-func InvariantProfile(k Keeper) sdk.Invariant {
+// formatOutputProfiles prepare invalid profiles to be displayed correctly
+func formatOutputProfiles(invalidProfiles types.Profiles) (outputProfiles string) {
+	outputProfiles = "Invalid profiles:\n"
+	for _, invalidProfile := range invalidProfiles {
+		outputProfiles += fmt.Sprintf("[Moniker]: %s, [Creator]: %s\n", invalidProfile.Moniker, invalidProfile.Creator)
+	}
+	return outputProfiles
+}
+
+// NonEmptyProfileCreatorOrMonikerInvariant checks that all registered profiles have a non-empty moniker and a non-empty creator
+func NonEmptyProfileCreatorOrMonikerInvariant(k Keeper) sdk.Invariant {
 	return func(ctx sdk.Context) (string, bool) {
-		return "", true
+		var invalidProfiles types.Profiles
+		k.IterateProfile(ctx, func(_ int64, profile types.Profile) (stop bool) {
+			if err := profile.Validate(); err != nil {
+				invalidProfiles = append(invalidProfiles, profile)
+			}
+			return false
+		})
+
+		return sdk.FormatInvariant(types.ModuleName, "invalid profiles",
+			fmt.Sprintf("The following list contains invalid profiles that have empty moniker, empty creator or both:\n %s",
+				formatOutputProfiles(invalidProfiles)),
+		), invalidProfiles != nil
 	}
 }
