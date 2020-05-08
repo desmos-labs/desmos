6a66aac1e4cd9c38895f42adf40540138f05bc5e
bragaz <leo.braga95@gmail.com>
2020-05-08 17:26:50 +0200

    implementing benchmarks

--- x/posts/internal/keeper/keeper_bench_test.go
+++ x/posts/internal/keeper/keeper_bench_test.go
@@ -1,9 +1,100 @@
-package keeper
+package keeper_test
 
-import "testing"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"github.com/desmos-labs/desmos/x/posts/internal/simulation"
+	"github.com/desmos-labs/desmos/x/posts/internal/types"
+	"math/rand"
+	"testing"
+)
+
+// RandomPostIDOrSubspace
+func RandomPostIDOrSubspace() types.PostID {
+	bytes := make([]byte, 128)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		panic(err)
+	}
+	hash := sha256.Sum256(bytes)
+	return types.PostID(hex.EncodeToString(hash[:]))
+}
+
+func RandomPosts() ([]types.Post, rand.Rand) {
+	r := rand.New(rand.NewSource(100))
+	return []types.Post{
+		types.NewPost(
+			RandomPostIDOrSubspace(),
+			RandomPostIDOrSubspace(),
+			"Message",
+			false,
+			RandomPostIDOrSubspace().String(),
+			map[string]string{},
+			testPost.Created,
+			testPost.Creator,
+		),
+		types.NewPost(
+			RandomPostIDOrSubspace(),
+			RandomPostIDOrSubspace(),
+			"Message",
+			false,
+			RandomPostIDOrSubspace().String(),
+			map[string]string{},
+			testPost.Created,
+			testPost.Creator,
+		).WithMedias(testPost.Medias),
+		types.NewPost(
+			RandomPostIDOrSubspace(),
+			RandomPostIDOrSubspace(),
+			"Message",
+			false,
+			RandomPostIDOrSubspace().String(),
+			map[string]string{},
+			testPost.Created,
+			testPost.Creator,
+		).WithMedias(simulation.RandomMedias(r)),
+		types.NewPost(
+			RandomPostIDOrSubspace(),
+			RandomPostIDOrSubspace(),
+			"Message",
+			false,
+			RandomPostIDOrSubspace().String(),
+			map[string]string{},
+			testPost.Created,
+			testPost.Creator,
+		).WithPollData(*simulation.RandomPollData(r)),
+		types.NewPost(
+			RandomPostIDOrSubspace(),
+			RandomPostIDOrSubspace(),
+			"Message",
+			false,
+			RandomPostIDOrSubspace().String(),
+			map[string]string{},
+			testPost.Created,
+			testPost.Creator,
+		).WithMedias(testPost.Medias).WithPollData(*simulation.RandomPollData(r)),
+		types.NewPost(
+			RandomPostIDOrSubspace(),
+			RandomPostIDOrSubspace(),
+			"Message",
+			false,
+			RandomPostIDOrSubspace().String(),
+			map[string]string{},
+			testPost.Created,
+			testPost.Creator,
+		).WithMedias(simulation.RandomMedias(r)).WithPollData(*simulation.RandomPollData(r)),
+	}, *r
+}
 
 func BenchmarkKeeper_SavePost(b *testing.B) {
+	ctx, k := SetupTestInput()
+	posts, r := RandomPosts()
 
+	r.Intn(len(posts))
+
+	for i := 0; i < b.N; i++ {
+
+	}
 }
 
 func BenchmarkKeeper_GetPost(b *testing.B) {
