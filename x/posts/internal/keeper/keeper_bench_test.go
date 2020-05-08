d5c358fa5f6f14cf030089959adc8715b4cf0de6
bragaz <leo.braga95@gmail.com>
2020-05-08 15:16:19 +0200

    started implementing posts' keeper benchmarks

---
+++ x/posts/internal/keeper/keeper_bench_test.go
@@ -0,0 +1,23 @@
+package keeper
+
+import "testing"
+
+func BenchmarkKeeper_SavePost(b *testing.B) {
+
+}
+
+func BenchmarkKeeper_GetPost(b *testing.B) {
+
+}
+
+func BenchmarkKeeper_GetPostsFiltered(b *testing.B) {
+
+}
+
+func BenchmarkKeeper_SavePostReaction(b *testing.B) {
+
+}
+
+func BenchmarkKeeper_GetPostReactions(b *testing.B) {
+
+}
