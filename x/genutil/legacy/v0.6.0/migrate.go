bd5fac07e86c091848613c0c248b0f292ef2cb94
riccardo.montagnin <riccardo.montagnin@gmail.com>
2020-05-13 08:30:58 +0200

    Fixed the migration form

--- x/genutil/legacy/v0.6.0/migrate.go
+++ x/genutil/legacy/v0.6.0/migrate.go
@@ -3,35 +3,40 @@ package v060
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/x/genutil"
+	v040posts "github.com/desmos-labs/desmos/x/posts/legacy/v0.4.0"
+	v060posts "github.com/desmos-labs/desmos/x/posts/legacy/v0.6.0"
+	v040profiles "github.com/desmos-labs/desmos/x/profile/legacy/v0.4.0"
+	v060profiles "github.com/desmos-labs/desmos/x/profile/legacy/v0.6.0"
+)
 
 // Migrate migrates exported state from v0.5.0 to a v0.6.0 genesis state.
 func Migrate(appState genutil.AppMap, _ ...interface{}) genutil.AppMap {
-  v050Codec := codec.New()
-	codec.RegisterCrypto(v040Codec)
-  
+	v050Codec := codec.New()
+	codec.RegisterCrypto(v050Codec)
+
 	v060Codec := codec.New()
 	codec.RegisterCrypto(v060Codec)
 
-  // Migrate posts state
+	// Migrate posts state
 	if appState[v040posts.ModuleName] != nil {
 		var genDocs v040posts.GenesisState
 		v050Codec.MustUnmarshalJSON(appState[v040posts.ModuleName], &genDocs)
 
-		appState[v040posts.ModuleName] = cdc.MustMarshalJSON(
+		appState[v040posts.ModuleName] = v060Codec.MustMarshalJSON(
 			v060posts.Migrate(genDocs),
 		)
-  }
-  
-  // Migrate profile state
+	}
+
+	// Migrate profile state
 	if appState[v040profiles.ModuleName] != nil {
 		var genDocs v040profiles.GenesisState
 		v050Codec.MustUnmarshalJSON(appState[v040profiles.ModuleName], &genDocs)
 
 		delete(appState, v040profiles.ModuleName)
-		appState[v060profiles.ModuleName] = cdc.MustMarshalJSON(
+		appState[v060profiles.ModuleName] = v060Codec.MustMarshalJSON(
 			genDocs,
 		)
 	}
-  
+
 	return appState
 }
