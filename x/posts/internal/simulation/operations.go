fcb7eabc85c690599ff7e64ee6baa1f8035bb2c9
bragaz <leo.braga95@gmail.com>
2020-05-29 16:26:37 +0200

    raising default gas for test

--- x/posts/internal/simulation/operations.go
+++ x/posts/internal/simulation/operations.go
@@ -19,7 +19,7 @@ const (
 	OpWeightMsgAnswerPoll       = "op_weight_msg_answer_poll"
 	OpWeightMsgRegisterReaction = "op_weight_msg_register_reaction"
 
-	DefaultGasValue = 600000
+	DefaultGasValue = 2000000
 )
 
 // WeightedOperations returns all the operations from the module with their respective weights
