c19b906e70945af08b9936cca9980d8a6e16d401
bragaz <leo.braga95@gmail.com>
2020-05-29 14:15:42 +0200

    moved back default gas to 500000 to let the post sim tests pass

--- x/posts/internal/simulation/operations.go
+++ x/posts/internal/simulation/operations.go
@@ -19,7 +19,7 @@ const (
 	OpWeightMsgAnswerPoll       = "op_weight_msg_answer_poll"
 	OpWeightMsgRegisterReaction = "op_weight_msg_register_reaction"
 
-	DefaultGasValue = 400000
+	DefaultGasValue = 500000
 )
 
 // WeightedOperations returns all the operations from the module with their respective weights
