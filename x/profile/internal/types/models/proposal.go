3a801c6e965ca3b61a6df1fb1cd02b6e6d2ccbc5
bragaz <leo.braga95@gmail.com>
2020-06-08 13:16:16 +0200

    adding methods to param

--- x/profile/internal/types/models/proposal.go
+++ x/profile/internal/types/models/proposal.go
@@ -1,5 +1,7 @@
 package models
 
+import "github.com/cosmos/cosmos-sdk/x/gov"
+
 const (
 	ProposalTypeNameSurnameParamsEdit string = "NameSurnameParamsEdit"
 	ProposalTypeMonikerParamsEdit     string = "MonikerParamsEdit"
@@ -10,4 +12,31 @@ type NameSurnameParamsEditProposal struct {
 	Title             string               `json:"title" yaml:"title"`
 	Description       string               `json:"description" yaml:"description"`
 	NameSurnameParams NameSurnameLenParams `json:"name_surname_params" yaml:"name_surname_params"`
+}
+
+var _ gov.Content = NameSurnameParamsEditProposal{}
+
+func init() {
+	gov.RegisterProposalType(ProposalTypeNameSurnameParamsEdit)
+	gov.RegisterProposalTypeCodec(NameSurnameParamsEditProposal{}, "desmos/NameSurnameProfileParamsEditProposal")
+}
+
+func (nsp NameSurnameParamsEditProposal) GetTitle() string {
+	return nsp.Title
+}
+
+func (nsp NameSurnameParamsEditProposal) GetDescription() string {
+	return nsp.Description
+}
+
+func (nsp NameSurnameParamsEditProposal) ProposalRoute() string {
+	return RouterKey
+}
+
+func (nsp NameSurnameParamsEditProposal) ProposalType() string {
+	return ProposalTypeNameSurnameParamsEdit
+}
+
+func (nsp NameSurnameParamsEditProposal) ValidateBasic() string {
+
 }
