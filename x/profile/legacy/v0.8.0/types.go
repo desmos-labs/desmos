9f1af662a8206b5fc65803e922e752217cd2ed41
riccardo.montagnin <riccardo.montagnin@gmail.com>
2020-06-15 14:40:06 +0200

    Fixed build errors

--- x/profile/legacy/v0.8.0/types.go
+++ x/profile/legacy/v0.8.0/types.go
@@ -1,9 +1,18 @@
 package v0_8_0
 
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
 type Profile struct {
 	DTag     string         `json:"dtag" yaml:"dtag"`
 	Moniker  *string        `json:"moniker,omitempty" yaml:"moniker,omitempty"`
 	Bio      *string        `json:"bio,omitempty" yaml:"bio,omitempty"`
 	Pictures *Pictures      `json:"pictures,omitempty" yaml:"pictures,omitempty"`
 	Creator  sdk.AccAddress `json:"creator" yaml:"creator"`
+}
+
+type Pictures struct {
+	Profile *string `json:"profile,omitempty" yaml:"profile,omitempty"`
+	Cover   *string `json:"cover,omitempty" yaml:"cover,omitempty"`
 }
