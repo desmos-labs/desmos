96578374ffa528700b4d5968f63757dbbb4ad5b5
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2020-07-17 21:40:42 +0200

    Error improvements
    See PR #238

--- x/posts/types/msgs/msg_reactions.go
+++ x/posts/types/msgs/msg_reactions.go
@@ -8,7 +8,9 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
-	postsModels "github.com/desmos-labs/desmos/x/posts/types/models"
+	commonerrors "github.com/desmos-labs/desmos/x/commons/types/errors"
+	postserrors "github.com/desmos-labs/desmos/x/posts/types/errors"
+	postsmodels "github.com/desmos-labs/desmos/x/posts/types/models"
 )
 
 // MsgRegisterReaction represents the message that must be used when wanting
@@ -31,27 +33,27 @@ func NewMsgRegisterReaction(creator sdk.AccAddress, shortCode, value, subspace s
 }
 
 // Route should return the name of the module
-func (msg MsgRegisterReaction) Route() string { return postsModels.RouterKey }
+func (msg MsgRegisterReaction) Route() string { return postsmodels.RouterKey }
 
 // Type should return the action
-func (msg MsgRegisterReaction) Type() string { return postsModels.ActionRegisterReaction }
+func (msg MsgRegisterReaction) Type() string { return postsmodels.ActionRegisterReaction }
 
 // ValidateBasic runs stateless checks on the message
 func (msg MsgRegisterReaction) ValidateBasic() error {
 	if msg.Creator.Empty() {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, fmt.Sprintf("Invalid creator address: %s", msg.Creator))
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, fmt.Sprintf("invalid creator address: %s", msg.Creator))
 	}
 
-	if !postsModels.ShortCodeRegEx.MatchString(msg.ShortCode) {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "The specified shortcode is not valid. To be valid it must only contains a-z, 0-9, - and _ and must start and end with a :")
+	if !postsmodels.ShortCodeRegEx.MatchString(msg.ShortCode) {
+		return sdkerrors.Wrap(postserrors.ErrInvalidReactionCode, msg.ShortCode)
 	}
 
 	if !commons.IsURIValid(msg.Value) {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "reaction value should be a valid URL")
+		return sdkerrors.Wrap(commonerrors.ErrInvalidURI, "reaction value should be a valid uri")
 	}
 
-	if !postsModels.Sha256RegEx.MatchString(msg.Subspace) {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "reaction subspace must be a valid sha-256 hash")
+	if !postsmodels.Sha256RegEx.MatchString(msg.Subspace) {
+		return sdkerrors.Wrap(postserrors.ErrInvalidSubspace, "reaction subspace must be a valid sha-256 hash")
 	}
 
 	return nil
