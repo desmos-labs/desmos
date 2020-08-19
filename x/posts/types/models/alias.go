b01492491b6375357b8a68a16f2593ebf978d088
Leonardo Bragagnolo <leo.braga95@gmail.com>
2020-08-19 08:46:45 +0200

    Replaced posts.ComputeID with inner ID computation
    See PR #255

--- x/posts/types/models/alias.go
+++ x/posts/types/models/alias.go
@@ -33,12 +33,12 @@ const (
 
 var (
 	// functions aliases
-	NewAttachment              = common.NewAttachment
-	NewAttachments             = common.NewAttachments
 	IsValidPostID              = common.IsValidPostID
 	IsValidSubspace            = common.IsValidSubspace
 	IsValidReactionCode        = common.IsValidReactionCode
 	GetEmojiByShortCodeOrValue = common.GetEmojiByShortCodeOrValue
+	NewAttachment              = common.NewAttachment
+	NewAttachments             = common.NewAttachments
 	ParseAnswerID              = polls.ParseAnswerID
 	NewPollAnswer              = polls.NewPollAnswer
 	NewPollAnswers             = polls.NewPollAnswers
@@ -64,10 +64,10 @@ var (
 )
 
 type (
-	Attachment    = common.Attachment
-	Attachments   = common.Attachments
 	OptionalData  = common.OptionalData
 	KeyValue      = common.KeyValue
+	Attachment    = common.Attachment
+	Attachments   = common.Attachments
 	AnswerID      = polls.AnswerID
 	PollAnswer    = polls.PollAnswer
 	PollAnswers   = polls.PollAnswers
