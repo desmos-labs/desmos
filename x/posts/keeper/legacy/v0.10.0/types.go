4b759573713ddb7d13ea45dcdfd1f32ba6848e40
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2020-09-28 12:30:44 +0200

    Version 0.12.1
    See PR #276

--- x/posts/keeper/legacy/v0.10.0/types.go
+++ x/posts/keeper/legacy/v0.10.0/types.go
@@ -1,6 +1,7 @@
 package v0100
 
 import (
+	"sort"
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -12,6 +13,46 @@ type PostID string
 // OptionalData represents a Posts' optional data and allows for custom
 // Amino and JSON serialization and deserialization.
 type OptionalData map[string]string
+
+// KeyValue is a simple key/value representation of one field of a OptionalData.
+type KeyValue struct {
+	Key   string
+	Value string
+}
+
+// MarshalAmino transforms the OptionalData to an array of key/value.
+func (m OptionalData) MarshalAmino() ([]KeyValue, error) {
+	fieldKeys := make([]string, len(m))
+	i := 0
+	for key := range m {
+		fieldKeys[i] = key
+		i++
+	}
+
+	sort.Stable(sort.StringSlice(fieldKeys))
+
+	p := make([]KeyValue, len(m))
+	for i, key := range fieldKeys {
+		p[i] = KeyValue{
+			Key:   key,
+			Value: m[key],
+		}
+	}
+
+	return p, nil
+}
+
+// UnmarshalAmino transforms the key/value array to a OptionalData.
+func (m *OptionalData) UnmarshalAmino(keyValues []KeyValue) error {
+	tempMap := make(map[string]string, len(keyValues))
+	for _, p := range keyValues {
+		tempMap[p.Key] = p.Value
+	}
+
+	*m = tempMap
+
+	return nil
+}
 
 // Attachment contains the information representing any type of file provided with a post.
 // This file can be an image or a multimedia file (vocals, video, documents, etc.).
