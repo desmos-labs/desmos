477eac8a0aa718f2236f28241e731686907466ba
bragaz <leo.braga95@gmail.com>
2020-10-13 18:27:17 +0200

    added gen util migration tests

--- x/genutil/legacy/v0.13.0/migrate_test.go
+++ x/genutil/legacy/v0.13.0/migrate_test.go
@@ -1,6 +1,51 @@
 package v0130_test
 
-import "testing"
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/genutil"
+	v0130 "github.com/desmos-labs/desmos/x/genutil/legacy/v0.13.0"
+	v0120posts "github.com/desmos-labs/desmos/x/posts/legacy/v0.12.0"
+	v0130posts "github.com/desmos-labs/desmos/x/posts/legacy/v0.13.0"
+	v060posts "github.com/desmos-labs/desmos/x/posts/legacy/v0.6.0"
+	"github.com/stretchr/testify/require"
+	tm "github.com/tendermint/tendermint/types"
+	"reflect"
+	"testing"
+)
 
 func TestMigrate0120(t *testing.T) {
+	cdc := codec.New()
+	codec.RegisterCrypto(cdc)
+
+	config := sdk.GetConfig()
+	config.SetBech32PrefixForAccount("desmos", "desmos"+sdk.PrefixPublic)
+	config.Seal()
+
+	// Read the genesis
+	genesis, err := tm.GenesisDocFromFile("v0120state.json")
+	require.NoError(t, err)
+
+	// Read the whole app state
+	var v012state genutil.AppMap
+	err = cdc.UnmarshalJSON(genesis.AppState, &v012state)
+	require.NoError(t, err)
+
+	// Migrate everything
+	v0130state := v0130.Migrate(v012state)
+
+	// Make sure that all the posts are migrated
+	var v012postsState v0120posts.GenesisState
+	err = cdc.UnmarshalJSON(v012state[v060posts.ModuleName], &v012postsState)
+	//require.NoError(t, err)
+
+	var v0130postsState v0130posts.GenesisState
+	err = cdc.UnmarshalJSON(v0130state[v060posts.ModuleName], &v0130postsState)
+	require.NoError(t, err)
+	require.Equal(t, len(v012postsState.Posts), len(v0130postsState.Posts))
+
+	// Make sure that all the posts' polls are migrated correctly
+	for _, post := range v0130postsState.Posts {
+		require.Equal(t, "OptionalData", reflect.TypeOf(post).String())
+	}
 }
--- x/posts/legacy/v0.13.0/migrate_test.go
+++ x/posts/legacy/v0.13.0/migrate_test.go
@@ -1,17 +1,16 @@
 package v0130_test
 
 import (
-	v0100posts "github.com/desmos-labs/desmos/x/posts/legacy/v0.10.0"
 	"testing"
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	v0100posts "github.com/desmos-labs/desmos/x/posts/legacy/v0.10.0"
 	v0120posts "github.com/desmos-labs/desmos/x/posts/legacy/v0.12.0"
 	v0130 "github.com/desmos-labs/desmos/x/posts/legacy/v0.13.0"
 	v0130posts "github.com/desmos-labs/desmos/x/posts/legacy/v0.13.0"
 	v040posts "github.com/desmos-labs/desmos/x/posts/legacy/v0.4.0"
 	v060 "github.com/desmos-labs/desmos/x/posts/legacy/v0.6.0"
-	"github.com/magiconair/properties/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -147,6 +146,81 @@ func TestMigrate(t *testing.T) {
 	}
 }
 
+func TestConvertPosts(t *testing.T) {
+	parentPostCreator, err := sdk.AccAddressFromBech32("cosmos1y54exmx84cqtasvjnskf9f63djuuj68p7hqf47")
+	require.NoError(t, err)
+
+	postCreator, err := sdk.AccAddressFromBech32("cosmos1cjf97gpzwmaf30pzvaargfgr884mpp5ak8f7ns")
+	require.NoError(t, err)
+
+	parentCreationTime := time.Now().UTC()
+	postCreationTime := parentCreationTime.Add(time.Hour)
+
+	subspace := "4e188d9c17150037d5199bbdb91ae1eb2a78a15aca04cb35530cccb81494b36e"
+
+	parentID := v040posts.ComputeID(parentCreationTime, parentPostCreator, subspace)
+	postID := v040posts.ComputeID(postCreationTime, postCreator, subspace)
+
+	var posts = []v0120posts.Post{
+		{
+			PostID:         parentID,
+			ParentID:       "",
+			Message:        "Message",
+			AllowsComments: true,
+			Subspace:       "4e188d9c17150037d5199bbdb91ae1eb2a78a15aca04cb35530cccb81494b36e",
+			OptionalData:   map[string]string{"optional": "data"},
+			Created:        parentCreationTime,
+			LastEdited:     time.Time{},
+			Creator:        parentPostCreator,
+			Attachments:    []v0100posts.Attachment{{URI: "https://uri.com", MimeType: "text/plain"}},
+		},
+		{
+			PostID:         postID,
+			ParentID:       parentID,
+			Message:        "Message",
+			AllowsComments: true,
+			Subspace:       "4e188d9c17150037d5199bbdb91ae1eb2a78a15aca04cb35530cccb81494b36e",
+			OptionalData:   map[string]string{"optional": "data"},
+			Created:        postCreationTime,
+			LastEdited:     time.Time{},
+			Creator:        postCreator,
+			Attachments:    []v0100posts.Attachment{{URI: "https://uri.com", MimeType: "text/plain"}},
+		},
+	}
+
+	var expectedPosts = []v0130posts.Post{
+		{
+			PostID:         parentID,
+			ParentID:       "",
+			Message:        "Message",
+			AllowsComments: true,
+			Subspace:       "4e188d9c17150037d5199bbdb91ae1eb2a78a15aca04cb35530cccb81494b36e",
+			OptionalData:   []v0130.OptionalData{{Key: "optional", Value: "data"}},
+			Created:        parentCreationTime,
+			LastEdited:     time.Time{},
+			Creator:        parentPostCreator,
+			Attachments:    []v0100posts.Attachment{{URI: "https://uri.com", MimeType: "text/plain", Tags: nil}},
+		},
+		{
+			PostID:         postID,
+			ParentID:       parentID,
+			Message:        "Message",
+			AllowsComments: true,
+			Subspace:       "4e188d9c17150037d5199bbdb91ae1eb2a78a15aca04cb35530cccb81494b36e",
+			OptionalData:   []v0130.OptionalData{{Key: "optional", Value: "data"}},
+			Created:        postCreationTime,
+			LastEdited:     time.Time{},
+			Creator:        postCreator,
+			Attachments:    []v0100posts.Attachment{{URI: "https://uri.com", MimeType: "text/plain", Tags: nil}},
+		},
+	}
+
+	convertedPosts := v0130.ConvertPosts(posts)
+
+	require.Equal(t, expectedPosts, convertedPosts)
+	require.Equal(t, len(expectedPosts), len(convertedPosts))
+}
+
 func TestConvertOptionalData(t *testing.T) {
 	oldOptionalData := v040posts.OptionalData{
 		"optional": "data",
@@ -166,5 +240,5 @@ func TestConvertOptionalData(t *testing.T) {
 
 	actualOptionalData := v0130.ConvertOptionalData(oldOptionalData)
 
-	assert.Equal(t, expOptionalData, actualOptionalData)
+	require.Equal(t, expOptionalData, actualOptionalData)
 }
