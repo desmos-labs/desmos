d8891d20db3ab77c03f25b5ae6f05c804735db8f
riccardo.montagnin <riccardo.montagnin@gmail.com>
2021-01-05 10:08:22 +0100

    Fixed logging not working
    Signed-off-by: riccardo.montagnin <riccardo.montagnin@gmail.com>

--- app/desmosd/main.go
+++ app/desmosd/main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"os"
 
+	"github.com/cosmos/cosmos-sdk/server"
+
+	"github.com/desmos-labs/desmos/app"
+
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
 	"github.com/desmos-labs/desmos/app/desmosd/cmd"
 )
 
 func main() {
 	rootCmd, _ := cmd.NewRootCmd()
-	if err := cmd.Execute(rootCmd); err != nil {
-		os.Exit(1)
+	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
+		switch e := err.(type) {
+		case server.ErrorCode:
+			os.Exit(e.Code)
+
+		default:
+			os.Exit(1)
+		}
 	}
 }
