419b66718814643da46644aeaf25bc405bda34ea
riccardo.montagnin <riccardo.montagnin@gmail.com>
2021-03-23 10:14:50 +0100

    Fixed lint errors
    Signed-off-by: riccardo.montagnin <riccardo.montagnin@gmail.com>

--- x/profiles/types/genesis.go
+++ x/profiles/types/genesis.go
@@ -32,24 +32,3 @@ func ValidateGenesis(data *GenesisState) error {
 
 	return nil
 }
-
-// containDuplicates tells whether or not the profiles slice contain duplicates of the given profile
-func containDuplicates(profiles []Profile, profile Profile) bool {
-	var count = 0
-	for _, p := range profiles {
-		if p.GetAddress().Equals(profile.GetAddress()) || p.Dtag == profile.Dtag {
-			count++
-		}
-	}
-	return count > 1
-}
-
-// profileExists tells whether the given profiles slice contain a profile associated to the given address
-func profileExists(profiles []Profile, address string) bool {
-	for _, profile := range profiles {
-		if profile.GetAddress().String() == address {
-			return true
-		}
-	}
-	return false
-}
