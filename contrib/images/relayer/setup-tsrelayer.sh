071201eed2bdd815e101ea7fe652ec67e76cd7ab
Paul <p22626262@gmail.com>
2021-05-07 23:42:50 +0800

    Fix scripts for updated tsrelayer

--- contrib/images/relayer/setup-tsrelayer.sh
+++ contrib/images/relayer/setup-tsrelayer.sh
@@ -7,7 +7,7 @@ sleep 10
 ibc-setup connect
 
 # Set channels
-ibc-setup channel --src=ibc0 --dest=ibc1 --src-port=transfer --dest-port=transfer --version=ics20-1
-ibc-setup channel --src=ibc0 --dest=ibc1 --src-port=links --dest-port=links --version=links-1
+ibc-setup channel --src-connection=connection-0 --dest-connection=connection-0 --src-port=transfer --dest-port=transfer --version=ics20-1
+ibc-setup channel --src-connection=connection-0 --dest-connection=connection-0 --src-port=links --dest-port=links --version=links-1
 
 ibc-relayer start --log-level=debug --poll 5
