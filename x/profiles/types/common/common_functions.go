664209de3ac8a494d1bd2e893a7f94ba1197b3dc
Bax995 <leo.braga95@gmail.com>
2021-05-17 14:42:39 +0200

    removed unused functions updated regex in docs

--- x/profiles/types/common/common_functions.go
+++
@@ -1,7 +0,0 @@
-package common
-
-// NewStrPtr allows to build a new string pointer starting
-// from a simple string value to setup tests more easily
-func NewStrPtr(value string) *string {
-	return &value
-}
