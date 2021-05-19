4ee5fbfa86b9ab18495c2d5335b25b2c1dcd7cb5
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2021-05-19 12:33:32 +0200

    Morpheus Apollo upgrade fix and test
    See PR #432

---
+++ x/profiles/legacy/v0160/params.go
@@ -0,0 +1,8 @@
+package v0160
+
+// Parameters store keys
+var (
+	MonikerLenParamsKey = []byte("MonikerParams")
+	DTagParamsKey       = []byte("DTagParams")
+	MaxBioLenParamsKey  = []byte("MaxBioLen")
+)
