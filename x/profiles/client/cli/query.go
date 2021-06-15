d35aa5cc9269089f5508e2874899493eaf64b52d
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2021-06-15 10:53:20 +0200

    Added DONTCOVER inside query.go and tx.go
    Signed-off-by: Riccardo Montagnin <riccardo.montagnin@gmail.com>

--- x/profiles/client/cli/query.go
+++ x/profiles/client/cli/query.go
@@ -7,6 +7,9 @@ import (
 	"github.com/desmos-labs/desmos/x/profiles/types"
 )
 
+// DONTCOVER
+// Tests will use single commands and not the global query one
+
 // GetQueryCmd returns the command allowing to perform queries
 func GetQueryCmd() *cobra.Command {
 	profileQueryCmd := &cobra.Command{
