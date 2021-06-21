f0f52fcc0091573524cb5a26f478e3f2d8fd4c02
HarleyAppleChoi <appleno56@gmail.com>
2021-06-21 13:33:02 +0100

    Update test case

--- x/profiles/client/cli/cli_relationships_test.go
+++ x/profiles/client/cli/cli_relationships_test.go
@@ -119,6 +119,10 @@ func (s *IntegrationTestSuite) TestCmdQueryUserBlocks() {
 						"Test block",
 						"9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08",
 					),
+				},				
+				Pagination: &query.PageResponse{
+					NextKey: nil,
+					Total:   0,
 				},
 			},
 		},
