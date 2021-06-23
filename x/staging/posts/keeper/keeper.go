ecd0110ddafd24f0ecccbf512b2a074b0409f17d
Paul <p22626262@gmail.com>
2021-06-24 00:46:58 +0800

    Update x/staging/posts/keeper/keeper.go
    Co-authored-by: Leonardo Bragagnolo <leo.braga95@gmail.com>

--- x/staging/posts/keeper/keeper.go
+++ x/staging/posts/keeper/keeper.go
@@ -94,7 +94,7 @@ func (k Keeper) GetPost(ctx sdk.Context, id string) (post types.Post, found bool
 	return post, true
 }
 
-// GetPostCommentIDs returns the IDs of all the comments associated to the post
+// GetPostCommentsIDs returns the IDs of all the comments associated to the post
 // having the given postID
 func (k Keeper) GetPostCommentIDs(ctx sdk.Context, postID string) []string {
 	ids := []string{}
