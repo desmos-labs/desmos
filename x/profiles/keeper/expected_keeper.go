c67a331779faa0881aa72419d34b32010ec907cd
Paul <p22626262@gmail.com>
2021-07-15 12:48:46 +0800

    Revert expected subspaces keeper

--- x/profiles/keeper/expected_keeper.go
+++ x/profiles/keeper/expected_keeper.go
@@ -45,5 +45,5 @@ type ScopedKeeper interface {
 // SubspacesKeeper defines the expected subspaces keeper
 type SubspacesKeeper interface {
 	IterateUnregisteredPairs(ctx sdk.Context, fn func(index int64, pair subspacestypes.UnregisteredPair) (stop bool))
-	DeleteSubspaceUnregisteredPair(ctx sdk.Context, user, subspaceID string)
+	DeleteSubspaceUnregisteredPair(ctx sdk.Context, subspaceID, user string)
 }
