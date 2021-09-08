2c0ecaa65b03bafe2a615cbbfec14db89cf9522a
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2021-09-08 08:19:03 +0200

    Reverted default server config gas price
    Signed-off-by: Riccardo Montagnin <riccardo.montagnin@gmail.com>

--- app/desmos/cmd/root.go
+++ app/desmos/cmd/root.go
@@ -125,8 +125,8 @@ func initAppConfig() (string, interface{}) {
 	// - if you set srvCfg.MinGasPrices non-empty, validators CAN tweak their
 	//   own app.toml to override, or use this default value.
 	//
-	// In simapp, we set the min gas prices to 0.
-	srvCfg.MinGasPrices = "0udsm"
+	// In Desmos, we set the min gas prices to 0.
+	srvCfg.MinGasPrices = "0stake"
 
 	customAppConfig := CustomAppConfig{
 		Config: *srvCfg,
