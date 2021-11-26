c51ecacb8c6c29e735510f13e11129a82b758473
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2021-11-26 07:43:04 +0100

    fix: vesting accounts not working properly (#679)
    ## Description
    This PR implements the fix for the vesting accounts to working anymore after the `v2.3.0` mainnet upgrade
    
    <!-- Add a description of the changes that this PR introduces and the files that
    are the most critical to review. -->
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is not applicable and
    please add links to any relevant follow up issues.*
    
    I have...
    
    - [x] included the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] added `!` to the type prefix if API or client breaking change
    - [x] targeted the correct branch (see [PR Targeting](https://github.com/desmos-labs/desmos/blob/master/CONTRIBUTING.md#pr-targeting))
    - [ ] provided a link to the relevant issue or specification
    - [ ] followed the guidelines for [building modules](https://docs.cosmos.network/v0.44/building-modules/intro.html)
    - [x] included the necessary unit and integration [tests](https://github.com/desmos-labs/desmos/blob/master/CONTRIBUTING.md#testing)
    - [x] added a changelog entry to `CHANGELOG.md`
    - [x] included comments for [documenting Go code](https://blog.golang.org/godoc)
    - [ ] updated the relevant documentation or specification
    - [x] reviewed "Files changed" and left comments if necessary
    - [ ] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable and please add
    your handle next to the items reviewed if you only reviewed selected items.*
    
    I have...
    
    - [ ] confirmed the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] confirmed `!` in the type prefix if API or client breaking change
    - [ ] confirmed all author checklist items have been addressed
    - [ ] reviewed state machine logic
    - [ ] reviewed API design and naming
    - [ ] reviewed documentation is accurate
    - [ ] reviewed tests and test coverage
    - [ ] manually tested (if applicable)

--- x/profiles/keeper/migrations.go
+++ x/profiles/keeper/migrations.go
@@ -2,7 +2,14 @@ package keeper
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	v043 "github.com/cosmos/cosmos-sdk/x/auth/legacy/v043"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/vesting/exported"
+	"github.com/gogo/protobuf/grpc"
+
+	"github.com/desmos-labs/desmos/v2/x/profiles/types"
 
 	v210 "github.com/desmos-labs/desmos/v2/x/profiles/legacy/v210"
 
@@ -13,15 +20,17 @@ import (
 
 // Migrator is a struct for handling in-place store migrations.
 type Migrator struct {
-	keeper Keeper
-	amino  *codec.LegacyAmino
+	keeper      Keeper
+	queryServer grpc.Server
+	amino       *codec.LegacyAmino
 }
 
 // NewMigrator returns a new Migrator
-func NewMigrator(keeper Keeper, amino *codec.LegacyAmino) Migrator {
+func NewMigrator(keeper Keeper, amino *codec.LegacyAmino, queryServer grpc.Server) Migrator {
 	return Migrator{
-		keeper: keeper,
-		amino:  amino,
+		keeper:      keeper,
+		amino:       amino,
+		queryServer: queryServer,
 	}
 }
 
@@ -33,4 +42,68 @@ func (m Migrator) Migrate1to2(ctx sdk.Context) error {
 // Migrate2to3 migrates from version 2 to 3.
 func (m Migrator) Migrate2to3(ctx sdk.Context) error {
 	return v210.MigrateStore(ctx, m.keeper.storeKey, m.keeper.cdc)
+}
+
+// Migrate3to4 migrates from version 3 to 4.
+func (m Migrator) Migrate3to4(ctx sdk.Context) error {
+	var iterErr error
+
+	m.keeper.ak.IterateAccounts(ctx, func(account authtypes.AccountI) (stop bool) {
+
+		// If the account is a profile, migrate the profile
+		if profile, ok := account.(*types.Profile); ok {
+			err := m.migrateProfile(ctx, profile)
+			if err != nil {
+				iterErr = err
+				return true
+			}
+			return false
+		}
+
+		// If the account is not a profile migrate it normally
+		wb, err := v043.MigrateAccount(ctx, account, m.queryServer)
+		if err != nil {
+			iterErr = err
+			return true
+		}
+
+		if wb == nil {
+			return false
+		}
+
+		m.keeper.ak.SetAccount(ctx, wb)
+		return false
+	})
+
+	return iterErr
+}
+
+func (m Migrator) migrateProfile(ctx sdk.Context, profile *types.Profile) error {
+	// Do not migrate those profiles that are not based on a VestingAccount
+	vestingAcc, ok := profile.GetAccount().(exported.VestingAccount)
+	if !ok {
+		return nil
+	}
+
+	// Migrate the underlying vesting account
+	wb, err := v043.MigrateAccount(ctx, vestingAcc, m.queryServer)
+	if err != nil {
+		return err
+	}
+
+	if wb == nil {
+		return nil
+	}
+
+	// Serialize the underlying vesting account back into the Profile
+	accAny, err := codectypes.NewAnyWithValue(wb)
+	if err != nil {
+		return err
+	}
+	profile.Account = accAny
+
+	// Store the new Profile as a VestingProfile instead of a common Profile
+	// This will grant that future operations that deal with VestingAccount instances are carried out properly
+	m.keeper.ak.SetAccount(ctx, profile)
+	return nil
 }
