a14dc55e8995754a9341677cb984687f693047b4
Paul <p22626262@gmail.com>
2022-03-03 17:04:38 +0800

    Add all the msgs to wasm type

--- x/subspaces/types/wasm.go
+++ x/subspaces/types/wasm.go
@@ -1,7 +1,16 @@
 package types
 
 type SubspacesMsg struct {
-	CreateSubspace *MsgCreateSubspace `json:"create_subspace"`
+	CreateSubspace          *MsgCreateSubspace          `json:"create_subspace"`
+	EditSubspace            *MsgEditSubspace            `json:"edit_subspace"`
+	DeleteSubspace          *MsgDeleteSubspace          `json:"delete_subspace"`
+	CreateUserGroup         *MsgCreateUserGroup         `json:"create_user_group"`
+	EditUserGroup           *MsgEditUserGroup           `json:"edit_user_group"`
+	SetUserGroupPermissions *MsgSetUserGroupPermissions `json:"set_user_group_permissions"`
+	DeleteUserGroup         *MsgDeleteUserGroup         `json:"delete_user_group"`
+	AddUserToUserGroup      *MsgAddUserToUserGroup      `json:"add_user_to_user_group"`
+	RemoveUserFromUserGroup *MsgRemoveUserFromUserGroup `json:"remove_user_from_user_group"`
+	SetUserPermissions      *MsgSetUserPermissions      `json:"set_user_permissions"`
 }
 
 type SubspacesQueryRequest struct {
