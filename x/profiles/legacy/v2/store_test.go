629fbb8a87f24410a5f6e70309dc316696267dbb
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2022-03-08 15:15:37 +0100

    fixed store tests
    Signed-off-by: Riccardo Montagnin <riccardo.montagnin@gmail.com>

--- x/profiles/legacy/v2/store_test.go
+++ x/profiles/legacy/v2/store_test.go
@@ -3,9 +3,17 @@ package v2_test
 import (
 	"testing"
 
+	"github.com/cosmos/cosmos-sdk/store"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
+	paramskeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
+	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	"github.com/tendermint/tendermint/libs/log"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	dbm "github.com/tendermint/tm-db"
+
 	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
 
-	"github.com/cosmos/cosmos-sdk/testutil"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 
@@ -14,9 +22,52 @@ import (
 	"github.com/desmos-labs/desmos/v2/x/relationships/types"
 )
 
+func buildContext(
+	keys map[string]*sdk.KVStoreKey, tKeys map[string]*sdk.TransientStoreKey, memKeys map[string]*sdk.MemoryStoreKey,
+) sdk.Context {
+	db := dbm.NewMemDB()
+	cms := store.NewCommitMultiStore(db)
+	for _, key := range keys {
+		cms.MountStoreWithDB(key, sdk.StoreTypeIAVL, db)
+	}
+	for _, tKey := range tKeys {
+		cms.MountStoreWithDB(tKey, sdk.StoreTypeTransient, db)
+	}
+	for _, memKey := range memKeys {
+		cms.MountStoreWithDB(memKey, sdk.StoreTypeMemory, nil)
+	}
+
+	err := cms.LoadLatestVersion()
+	if err != nil {
+		panic(err)
+	}
+
+	return sdk.NewContext(cms, tmproto.Header{}, false, log.NewNopLogger())
+}
+
 func TestMigrateStore(t *testing.T) {
-	cdc, _ := app.MakeCodecs()
-	storeKey := sdk.NewKVStoreKey(types.StoreKey)
+	cdc, legacyAmino := app.MakeCodecs()
+
+	// Build all the necessary keys
+	keys := sdk.NewKVStoreKeys(authtypes.StoreKey, types.StoreKey)
+	tKeys := sdk.NewTransientStoreKeys(paramstypes.TStoreKey)
+	memKeys := sdk.NewMemoryStoreKeys(capabilitytypes.MemStoreKey)
+
+	// Build the x/auth keeper
+	paramsKeeper := paramskeeper.NewKeeper(
+		cdc,
+		legacyAmino,
+		keys[paramstypes.StoreKey],
+		tKeys[paramstypes.TStoreKey],
+	)
+	authKeeper := authkeeper.NewAccountKeeper(
+		cdc,
+		keys[authtypes.StoreKey],
+		paramsKeeper.Subspace(authtypes.ModuleName),
+		authtypes.ProtoBaseAccount,
+		app.GetMaccPerms(),
+	)
+
 	testCases := []struct {
 		name      string
 		store     func(ctx sdk.Context)
@@ -26,7 +77,7 @@ func TestMigrateStore(t *testing.T) {
 		{
 			name: "valid data is migrated properly",
 			store: func(ctx sdk.Context) {
-				store := ctx.KVStore(storeKey)
+				store := ctx.KVStore(keys[types.StoreKey])
 
 				blockBz := cdc.MustMarshal(&v2.UserBlock{
 					Blocker:    "blocker",
@@ -52,7 +103,7 @@ func TestMigrateStore(t *testing.T) {
 			},
 			shouldErr: false,
 			check: func(ctx sdk.Context) {
-				store := ctx.KVStore(storeKey)
+				store := ctx.KVStore(keys[types.StoreKey])
 
 				// Make sure all blocks are deleted
 				require.False(t, store.Has(v2.UserBlockStoreKey("blocker", "", "blocked")))
@@ -67,13 +118,12 @@ func TestMigrateStore(t *testing.T) {
 	for _, tc := range testCases {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
-			ctx := testutil.DefaultContext(storeKey, sdk.NewTransientStoreKey("test"))
+			ctx := buildContext(keys, tKeys, memKeys)
 			if tc.store != nil {
 				tc.store(ctx)
 			}
 
-			// TODO: Get the real account keeper and amino here
-			err := v2.MigrateStore(ctx, authkeeper.AccountKeeper{}, storeKey, nil, cdc)
+			err := v2.MigrateStore(ctx, authKeeper, keys[types.StoreKey], legacyAmino, cdc)
 			if tc.shouldErr {
 				require.Error(t, err)
 			} else {
