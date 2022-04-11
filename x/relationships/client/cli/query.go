59a515acf2117882018c092457ab0dc906488379
bragaz <leo.braga95@gmail.com>
2022-04-11 16:59:54 +0200

    added examples in CLI commands (tx and queries)

--- x/relationships/client/cli/query.go
+++ x/relationships/client/cli/query.go
@@ -2,9 +2,11 @@ package cli
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/version"
 	"github.com/spf13/cobra"
 
 	subspacestypes "github.com/desmos-labs/desmos/v3/x/subspaces/types"
@@ -33,7 +35,11 @@ func GetCmdQueryRelationships() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "relationships [subspace-id] [[creator]] [[counterparty]]",
 		Short: "Retrieve all the relationships inside a given subspace, with optional creator and counterparty",
-		Args:  cobra.RangeArgs(1, 3),
+		Example: fmt.Sprintf(`%s query relationships relationships 1
+%s query relationships relationships 1 desmos13p5pamrljhza3fp4es5m3llgmnde5fzcpq6nud
+%s query relationships relationships 1 desmos13p5pamrljhza3fp4es5m3llgmnde5fzcpq6nud desmos159axlj0mkvch02f95t5tkghychyeueaslk6r8f`,
+			version.AppName, version.AppName, version.AppName),
+		Args: cobra.RangeArgs(1, 3),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientQueryContext(cmd)
 			if err != nil {
@@ -84,7 +90,11 @@ func GetCmdQueryBlocks() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "blocks [subspace-id] [[blocker]] [[blocked]]",
 		Short: "Retrieve the list of all the user blocks present inside the given subspace with optional blocker and blocked addresses",
-		Args:  cobra.RangeArgs(1, 3),
+		Example: fmt.Sprintf(`%s query relationships blocks 1
+%s query relationships blocks 1 desmos13p5pamrljhza3fp4es5m3llgmnde5fzcpq6nud
+%s query relationships blocks 1 desmos13p5pamrljhza3fp4es5m3llgmnde5fzcpq6nud desmos159axlj0mkvch02f95t5tkghychyeueaslk6r8f`,
+			version.AppName, version.AppName, version.AppName),
+		Args: cobra.RangeArgs(1, 3),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientQueryContext(cmd)
 			if err != nil {
--- x/supply/client/cli/query.go
+++ x/supply/client/cli/query.go
@@ -35,9 +35,12 @@ func GetQueryCmd() *cobra.Command {
 // converted with a divider powered with the given (optional) divider_exponent
 func GetCmdQueryTotalSupply() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "total-supply [denom] [[divider_exponent]]",
-		Short:   "Query the total supply of the given denom. It can be converted with an optional 10 divider powered with the given divider_exponent",
-		Example: fmt.Sprintf("%s query supply total-supply [denom] [[divider_exponent]]", version.AppName),
+		Use:   "total-supply [denom] [[divider_exponent]]",
+		Short: "Query the total supply of the given denom. It can be converted with an optional 10 divider powered with the given divider_exponent",
+		Example: fmt.Sprintf(
+			`%s query supply total-supply "stake"
+%s query supply total-supply "stake" 5
+`, version.AppName, version.AppName),
 		Long: `
 Get the total supply of a token with the given denom. 
 If a divider exponent is given, the returned result will be divided by 10^(divider_exponent).
@@ -82,8 +85,9 @@ func GetCmdQueryCirculatingSupply() *cobra.Command {
 		Use:   "circulating-supply [denom] [[divider_exponent]]",
 		Short: "Query the circulating supply of the given denom. It can be converted with an optional 10 divider powered with the given divider_exponent",
 		Example: fmt.Sprintf(
-			"%s query supply circulating-supply [denom] [[divider_exponent]]", version.AppName,
-		),
+			`%s query supply circulating-supply "stake"
+%s query supply circulating-supply "stake" 5
+`, version.AppName, version.AppName),
 		Long: fmt.Sprintf(`
 Get the circulating supply of a token with the given denom. The result can be converted with an optional 10 divider powered by the given divider_exponent. 
 If the default value is kept, the result will be displayed in millionth (the common way with which tokens' amount
