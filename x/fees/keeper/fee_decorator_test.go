c6febea5d20ee6068505d204f4a28150f337250f
bragaz <leo.braga95@gmail.com>
2022-04-12 11:51:19 +0200

    added missing test case

--- x/fees/keeper/fee_decorator_test.go
+++ x/fees/keeper/fee_decorator_test.go
@@ -11,10 +11,12 @@ import (
 
 func (suite *KeeperTestSuite) TestFeeDecorator() {
 	testsCases := []struct {
-		name      string
-		store     func(ctx sdk.Context)
-		tx        sdk.Tx
-		shouldErr bool
+		name       string
+		store      func(ctx sdk.Context)
+		tx         sdk.Tx
+		shouldErr  bool
+		zeroHeight bool
+		simulate   bool
 	}{
 		{
 			name: "not existing min fees returns no error",
@@ -27,7 +29,9 @@ func (suite *KeeperTestSuite) TestFeeDecorator() {
 				nil,
 				"",
 			),
-			shouldErr: false,
+			shouldErr:  false,
+			zeroHeight: false,
+			simulate:   true,
 		},
 		{
 			name: "not enough fees returns error",
@@ -47,7 +51,31 @@ func (suite *KeeperTestSuite) TestFeeDecorator() {
 				nil,
 				"",
 			),
-			shouldErr: true,
+			shouldErr:  true,
+			zeroHeight: false,
+			simulate:   true,
+		},
+		{
+			name: "block height 0 skips minimum fees checks",
+			store: func(ctx sdk.Context) {
+				suite.keeper.SetParams(ctx, types.NewParams(
+					[]types.MinFee{
+						types.NewMinFee(
+							sdk.MsgTypeURL(&profilestypes.MsgSaveProfile{}),
+							sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(10))),
+						),
+					},
+				))
+			},
+			tx: legacytx.NewStdTx(
+				[]sdk.Msg{profilestypes.NewMsgSaveProfile("", "", "", "", "", "")},
+				legacytx.NewStdFee(10, sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 5))),
+				nil,
+				"",
+			),
+			shouldErr:  false,
+			simulate:   false,
+			zeroHeight: true,
 		},
 		{
 			name: "enough fees returns no error",
@@ -67,18 +95,23 @@ func (suite *KeeperTestSuite) TestFeeDecorator() {
 				nil,
 				"",
 			),
-			shouldErr: false,
+			shouldErr:  false,
+			zeroHeight: false,
+			simulate:   true,
 		},
 	}
 
 	for _, tc := range testsCases {
 		ctx, _ := suite.ctx.CacheContext()
 		if tc.store != nil {
+			if tc.zeroHeight {
+				ctx = suite.ctx.WithBlockHeight(0)
+			}
 			tc.store(ctx)
 		}
 
 		anteHandler := sdk.ChainAnteDecorators(keeper.NewMinFeeDecorator(suite.keeper))
-		_, err := anteHandler(ctx, tc.tx, true)
+		_, err := anteHandler(ctx, tc.tx, tc.simulate)
 		if tc.shouldErr {
 			suite.Require().Error(err)
 		} else {
