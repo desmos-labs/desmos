39d5607b08a29ca5be81783cc4e29242a4202c00
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2022-04-13 10:49:48 +0200

    updated fee decorator check
    Signed-off-by: Riccardo Montagnin <riccardo.montagnin@gmail.com>

--- x/fees/keeper/fee_decorator.go
+++ x/fees/keeper/fee_decorator.go
@@ -16,14 +16,16 @@ func NewMinFeeDecorator(feesKeeper Keeper) MinFeeDecorator {
 }
 
 func (mfd MinFeeDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
-	// all transactions must be of type feeTx
+	// All transactions must be of type feeTx
 	feeTx, ok := tx.(sdk.FeeTx)
 	if !ok {
 		return newCtx, sdkerrors.Wrap(sdkerrors.ErrTxDecode, "Tx must be a FeeTx")
 	}
 
-	// skip block with height 0, otherwise no chain initialization could happen!
-	if ctx.BlockHeight() == 0 && !simulate {
+	// Skip block with height 0 since this handler will be called before the fee params are initialized,
+	// which would cause a panic when trying to start the chain
+	// TODO: When there is a better params handling, remove this and simply rely on empty params instead
+	if ctx.BlockHeight() == 0 {
 		return next(ctx, tx, simulate)
 	}
 
