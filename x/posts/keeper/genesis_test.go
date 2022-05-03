711d89d1eb86c54b8c47b44bbfea25cff35c7f49
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2022-05-03 09:35:38 +0200

    added import genesis tests
    Signed-off-by: Riccardo Montagnin <riccardo.montagnin@gmail.com>

--- x/posts/keeper/genesis_test.go
+++ x/posts/keeper/genesis_test.go
@@ -3,6 +3,8 @@ package keeper_test
 import (
 	"time"
 
+	subspacestypes "github.com/desmos-labs/desmos/v3/x/subspaces/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/desmos-labs/desmos/v3/x/posts/types"
@@ -171,6 +173,275 @@ func (suite *KeeperTestsuite) TestKeeper_ExportGenesis() {
 
 			genesis := suite.k.ExportGenesis(ctx)
 			suite.Require().Equal(tc.expGenesis, genesis)
+		})
+	}
+}
+
+func (suite *KeeperTestsuite) TestKeeper_ImportGenesis() {
+	user, err := sdk.AccAddressFromBech32("cosmos1vs8dps0ktst5ekynmszxuxphfq08rhmepsn8st")
+	suite.Require().NoError(err)
+
+	testCases := []struct {
+		name      string
+		store     func(ctx sdk.Context)
+		data      types.GenesisState
+		shouldErr bool
+		check     func(ctx sdk.Context)
+	}{
+		{
+			name: "non existing subspace returns error while importing data",
+			data: types.GenesisState{
+				SubspacesData: []types.SubspaceDataEntry{
+					types.NewSubspaceDataEntry(1, 1),
+				},
+			},
+			shouldErr: true,
+		},
+		{
+			name: "subspace data is imported properly",
+			store: func(ctx sdk.Context) {
+				suite.sk.SaveSubspace(ctx, subspacestypes.NewSubspace(
+					1,
+					"Test subspace",
+					"This is a test subspace",
+					"cosmos1qzskhrcjnkdz2ln4yeafzsdwht8ch08j4wed69",
+					"cosmos1m0czrla04f7rp3zg7dsgc4kla54q7pc4xt00l5",
+					"cosmos1qzskhrcjnkdz2ln4yeafzsdwht8ch08j4wed69",
+					time.Date(2020, 1, 1, 12, 00, 00, 000, time.UTC),
+				))
+			},
+			data: types.GenesisState{
+				SubspacesData: []types.SubspaceDataEntry{
+					types.NewSubspaceDataEntry(1, 1),
+				},
+			},
+			shouldErr: false,
+			check: func(ctx sdk.Context) {
+				stored, err := suite.k.GetPostID(ctx, 1)
+				suite.Require().NoError(err)
+				suite.Require().Equal(uint64(1), stored)
+			},
+		},
+		{
+			name: "non existing subspace returns error while importing post",
+			data: types.GenesisState{
+				GenesisPosts: []types.GenesisPost{
+					types.NewGenesisPost(1, types.NewPost(
+						1,
+						1,
+						"External ID",
+						"This is a text",
+						"cosmos13t6y2nnugtshwuy0zkrq287a95lyy8vzleaxmd",
+						1,
+						nil,
+						nil,
+						types.REPLY_SETTING_EVERYONE,
+						time.Date(2020, 1, 1, 12, 00, 00, 000, time.UTC),
+						nil,
+					)),
+				},
+			},
+			shouldErr: true,
+		},
+		{
+			name: "genesis post is imported correctly",
+			store: func(ctx sdk.Context) {
+				suite.sk.SaveSubspace(ctx, subspacestypes.NewSubspace(
+					1,
+					"Test subspace",
+					"This is a test subspace",
+					"cosmos1qzskhrcjnkdz2ln4yeafzsdwht8ch08j4wed69",
+					"cosmos1m0czrla04f7rp3zg7dsgc4kla54q7pc4xt00l5",
+					"cosmos1qzskhrcjnkdz2ln4yeafzsdwht8ch08j4wed69",
+					time.Date(2020, 1, 1, 12, 00, 00, 000, time.UTC),
+				))
+			},
+			data: types.GenesisState{
+				GenesisPosts: []types.GenesisPost{
+					types.NewGenesisPost(2, types.NewPost(
+						1,
+						1,
+						"External ID",
+						"This is a text",
+						"cosmos13t6y2nnugtshwuy0zkrq287a95lyy8vzleaxmd",
+						1,
+						nil,
+						nil,
+						types.REPLY_SETTING_EVERYONE,
+						time.Date(2020, 1, 1, 12, 00, 00, 000, time.UTC),
+						nil,
+					)),
+				},
+			},
+			shouldErr: false,
+			check: func(ctx sdk.Context) {
+				post, found := suite.k.GetPost(ctx, 1, 1)
+				suite.Require().True(found)
+				suite.Require().Equal(types.NewPost(
+					1,
+					1,
+					"External ID",
+					"This is a text",
+					"cosmos13t6y2nnugtshwuy0zkrq287a95lyy8vzleaxmd",
+					1,
+					nil,
+					nil,
+					types.REPLY_SETTING_EVERYONE,
+					time.Date(2020, 1, 1, 12, 00, 00, 000, time.UTC),
+					nil,
+				), post)
+
+				attachmentID, err := suite.k.GetAttachmentID(ctx, 1, 1)
+				suite.Require().NoError(err)
+				suite.Require().Equal(uint32(2), attachmentID)
+			},
+		},
+		{
+			name: "non existing post returns error while importing attachment",
+			data: types.GenesisState{
+				Attachments: []types.Attachment{
+					types.NewAttachment(1, 1, 1, types.NewMedia(
+						"ftp://user:password@example.com/image.png",
+						"image/png",
+					)),
+				},
+			},
+			shouldErr: true,
+		},
+		{
+			name: "attachment is imported correctly",
+			store: func(ctx sdk.Context) {
+				suite.sk.SaveSubspace(ctx, subspacestypes.NewSubspace(
+					1,
+					"Test subspace",
+					"This is a test subspace",
+					"cosmos1qzskhrcjnkdz2ln4yeafzsdwht8ch08j4wed69",
+					"cosmos1m0czrla04f7rp3zg7dsgc4kla54q7pc4xt00l5",
+					"cosmos1qzskhrcjnkdz2ln4yeafzsdwht8ch08j4wed69",
+					time.Date(2020, 1, 1, 12, 00, 00, 000, time.UTC),
+				))
+
+				suite.k.SavePost(ctx, types.NewPost(
+					1,
+					1,
+					"External ID",
+					"This is a text",
+					"cosmos13t6y2nnugtshwuy0zkrq287a95lyy8vzleaxmd",
+					1,
+					nil,
+					nil,
+					types.REPLY_SETTING_EVERYONE,
+					time.Date(2020, 1, 1, 12, 00, 00, 000, time.UTC),
+					nil,
+				))
+			},
+			data: types.GenesisState{
+				Attachments: []types.Attachment{
+					types.NewAttachment(1, 1, 1, types.NewMedia(
+						"ftp://user:password@example.com/image.png",
+						"image/png",
+					)),
+				},
+			},
+			shouldErr: false,
+			check: func(ctx sdk.Context) {
+				stored, found := suite.k.GetAttachment(ctx, 1, 1, 1)
+				suite.Require().True(found)
+				suite.Require().Equal(types.NewAttachment(1, 1, 1, types.NewMedia(
+					"ftp://user:password@example.com/image.png",
+					"image/png",
+				)), stored)
+			},
+		},
+		{
+			name: "non existing poll returns error when importing user answer",
+			data: types.GenesisState{
+				UserAnswers: []types.UserAnswer{
+					types.NewUserAnswer(1, 1, 1, []uint32{1}, user),
+				},
+			},
+			shouldErr: true,
+		},
+		{
+			name: "user answer is imported properly",
+			store: func(ctx sdk.Context) {
+				suite.sk.SaveSubspace(ctx, subspacestypes.NewSubspace(
+					1,
+					"Test subspace",
+					"This is a test subspace",
+					"cosmos1qzskhrcjnkdz2ln4yeafzsdwht8ch08j4wed69",
+					"cosmos1m0czrla04f7rp3zg7dsgc4kla54q7pc4xt00l5",
+					"cosmos1qzskhrcjnkdz2ln4yeafzsdwht8ch08j4wed69",
+					time.Date(2020, 1, 1, 12, 00, 00, 000, time.UTC),
+				))
+
+				suite.k.SavePost(ctx, types.NewPost(
+					1,
+					1,
+					"External ID",
+					"This is a text",
+					"cosmos13t6y2nnugtshwuy0zkrq287a95lyy8vzleaxmd",
+					1,
+					nil,
+					nil,
+					types.REPLY_SETTING_EVERYONE,
+					time.Date(2020, 1, 1, 12, 00, 00, 000, time.UTC),
+					nil,
+				))
+
+				suite.k.SaveAttachment(ctx, types.NewAttachment(1, 1, 1, types.NewPoll(
+					"What animal is best?",
+					[]types.Poll_ProvidedAnswer{
+						types.NewProvidedAnswer("Cat", nil),
+						types.NewProvidedAnswer("Dog", nil),
+					},
+					time.Date(2020, 1, 1, 12, 00, 00, 000, time.UTC),
+					false,
+					false,
+				)))
+			},
+			data: types.GenesisState{
+				UserAnswers: []types.UserAnswer{
+					types.NewUserAnswer(1, 1, 1, []uint32{1}, user),
+				},
+			},
+			shouldErr: false,
+			check: func(ctx sdk.Context) {
+				stored, found := suite.k.GetUserAnswer(ctx, 1, 1, 1, user)
+				suite.Require().True(found)
+				suite.Require().Equal(types.NewUserAnswer(1, 1, 1, []uint32{1}, user), stored)
+			},
+		},
+		{
+			name: "params are imported properly",
+			data: types.GenesisState{
+				Params: types.NewParams(200),
+			},
+			shouldErr: false,
+			check: func(ctx sdk.Context) {
+				stored := suite.k.GetParams(ctx)
+				suite.Require().Equal(types.NewParams(200), stored)
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		suite.Run(tc.name, func() {
+			ctx, _ := suite.ctx.CacheContext()
+			if tc.store != nil {
+				tc.store(ctx)
+			}
+
+			if tc.shouldErr {
+				suite.Require().Panics(func() { suite.k.InitGenesis(ctx, tc.data) })
+			} else {
+				suite.Require().NotPanics(func() { suite.k.InitGenesis(ctx, tc.data) })
+				if tc.check != nil {
+					tc.check(ctx)
+				}
+			}
+
 		})
 	}
 }
