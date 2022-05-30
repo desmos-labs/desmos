944a18a5e6af2685005a66a7d3abdd90d38f2cff
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2022-05-30 15:16:55 +0200

    added the upgrade details
    Signed-off-by: Riccardo Montagnin <riccardo.montagnin@gmail.com>

--- app/app.go
+++ app/app.go
@@ -11,7 +11,7 @@ import (
 	"github.com/desmos-labs/desmos/v3/app/upgrades"
 	v300 "github.com/desmos-labs/desmos/v3/app/upgrades/v300"
 	v310 "github.com/desmos-labs/desmos/v3/app/upgrades/v310"
-
+	v320 "github.com/desmos-labs/desmos/v3/app/upgrades/v320"
 	profilesv4 "github.com/desmos-labs/desmos/v3/x/profiles/legacy/v4"
 
 	"github.com/desmos-labs/desmos/v3/x/relationships"
@@ -949,6 +949,7 @@ func (app *DesmosApp) RegisterTendermintService(clientCtx client.Context) {
 func (app *DesmosApp) registerUpgradeHandlers() {
 	app.registerUpgrade(v300.NewUpgrade(app.mm, app.configurator))
 	app.registerUpgrade(v310.NewUpgrade(app.mm, app.configurator))
+	app.registerUpgrade(v320.NewUpgrade(app.mm, app.configurator))
 }
 
 // registerUpgrade registers the given upgrade to be supported by the app
