a2e9df11085b88ce0b5c52116f5946e403b10ac5
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2022-06-08 12:03:24 +0200

    added gRPC query methods implementation
    Signed-off-by: Riccardo Montagnin <riccardo.montagnin@gmail.com>

---
+++ x/reactions/keeper/grpc_query.go
@@ -0,0 +1,95 @@
+package keeper
+
+import (
+	"context"
+
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/types/query"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/desmos-labs/desmos/v3/x/reactions/types"
+)
+
+var _ types.QueryServer = &Keeper{}
+
+// Reactions implements the QueryReactions gRPC method
+func (k Keeper) Reactions(ctx context.Context, request *types.QueryReactionsRequest) (*types.QueryReactionsResponse, error) {
+	if request.SubspaceId == 0 {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "invalid subspace id")
+	}
+
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+	store := sdkCtx.KVStore(k.storeKey)
+
+	storePrefix := types.SubspaceReactionsPrefix(request.SubspaceId)
+	if request.PostId != 0 {
+		storePrefix = types.PostReactionsPrefix(request.SubspaceId, request.PostId)
+	}
+	reactionsStore := prefix.NewStore(store, storePrefix)
+
+	var reactions []types.Reaction
+	pageRes, err := query.Paginate(reactionsStore, request.Pagination, func(key []byte, value []byte) error {
+		var reaction types.Reaction
+		if err := k.cdc.Unmarshal(value, &reaction); err != nil {
+			return status.Error(codes.Internal, err.Error())
+		}
+
+		reactions = append(reactions, reaction)
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryReactionsResponse{Reactions: reactions, Pagination: pageRes}, nil
+}
+
+// RegisteredReactions implements the QueryRegisteredReactions gRPC method
+func (k Keeper) RegisteredReactions(ctx context.Context, request *types.QueryRegisteredReactionsRequest) (*types.QueryRegisteredReactionsResponse, error) {
+	if request.SubspaceId == 0 {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "invalid subspace id")
+	}
+
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+	store := sdkCtx.KVStore(k.storeKey)
+
+	storePrefix := types.SubspaceRegisteredReactionsPrefix(request.SubspaceId)
+	reactionsStore := prefix.NewStore(store, storePrefix)
+
+	var reactions []types.RegisteredReaction
+	pageRes, err := query.Paginate(reactionsStore, request.Pagination, func(key []byte, value []byte) error {
+		var reaction types.RegisteredReaction
+		if err := k.cdc.Unmarshal(value, &reaction); err != nil {
+			return status.Error(codes.Internal, err.Error())
+		}
+
+		reactions = append(reactions, reaction)
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryRegisteredReactionsResponse{RegisteredReactions: reactions, Pagination: pageRes}, nil
+}
+
+// ReactionsParams implements the QueryReactionsParams gRPC method
+func (k Keeper) ReactionsParams(ctx context.Context, request *types.QueryReactionsParamsRequest) (*types.QueryReactionsParamsResponse, error) {
+	if request.SubspaceId == 0 {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "invalid subspace id")
+	}
+
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+
+	params, err := k.GetSubspaceReactionsParams(sdkCtx, request.SubspaceId)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryReactionsParamsResponse{Params: params}, nil
+}
