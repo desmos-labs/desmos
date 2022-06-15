572fa3d52804e39fa226ef65eecf0f27b85ae684
Paul <p22626262@gmail.com>
2022-06-15 17:56:11 +0800

    Update x/posts/wasm/msg_parser_test.go

--- x/posts/wasm/msg_parser_test.go
+++ x/posts/wasm/msg_parser_test.go
@@ -84,7 +84,7 @@ func TestMsgsParser_ParseCustomMsgs(t *testing.T) {
 			)},
 		},
 		{
-			name: "delete post transfer json message is parsed correctly",
+			name: "delete post json message is parsed correctly",
 			msg: buildDeletePostRequest(cdc, types.NewMsgDeletePost(
 				1,
 				1,
