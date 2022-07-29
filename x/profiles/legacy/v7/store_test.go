d0004962642d4acbb58350e95c400cf4c5a509d6
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2022-07-29 09:12:35 +0200

    fix: fix profiles params upgrade
    Signed-off-by: Riccardo Montagnin <riccardo.montagnin@gmail.com>

---
+++ x/profiles/legacy/v7/store_test.go
@@ -0,0 +1,103 @@
+package v7_test
+
+import (
+	"testing"
+
+	v7 "github.com/desmos-labs/desmos/v4/x/profiles/legacy/v7"
+
+	"github.com/cosmos/cosmos-sdk/store"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
+	paramskeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
+	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	"github.com/tendermint/tendermint/libs/log"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	dbm "github.com/tendermint/tm-db"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+
+	"github.com/desmos-labs/desmos/v4/app"
+	"github.com/desmos-labs/desmos/v4/x/profiles/types"
+)
+
+func buildContext(
+	keys map[string]*sdk.KVStoreKey, tKeys map[string]*sdk.TransientStoreKey, memKeys map[string]*sdk.MemoryStoreKey,
+) sdk.Context {
+	db := dbm.NewMemDB()
+	cms := store.NewCommitMultiStore(db)
+	for _, key := range keys {
+		cms.MountStoreWithDB(key, sdk.StoreTypeIAVL, db)
+	}
+	for _, tKey := range tKeys {
+		cms.MountStoreWithDB(tKey, sdk.StoreTypeTransient, db)
+	}
+	for _, memKey := range memKeys {
+		cms.MountStoreWithDB(memKey, sdk.StoreTypeMemory, nil)
+	}
+
+	err := cms.LoadLatestVersion()
+	if err != nil {
+		panic(err)
+	}
+
+	return sdk.NewContext(cms, tmproto.Header{}, false, log.NewNopLogger())
+}
+
+func TestMigrateStore(t *testing.T) {
+	cdc, legacyAmino := app.MakeCodecs()
+
+	// Build all the necessary keys
+	keys := sdk.NewKVStoreKeys(authtypes.StoreKey, paramstypes.StoreKey, types.StoreKey)
+	tKeys := sdk.NewTransientStoreKeys(paramstypes.TStoreKey)
+	memKeys := sdk.NewMemoryStoreKeys(capabilitytypes.MemStoreKey)
+
+	// Get the params subspace
+	paramsKeeper := paramskeeper.NewKeeper(cdc, legacyAmino, keys[paramstypes.StoreKey], tKeys[paramstypes.TStoreKey])
+	paramsSubspace := paramsKeeper.Subspace(types.ModuleName)
+	paramsSubspace = paramsSubspace.WithKeyTable(types.ParamKeyTable())
+
+	testCases := []struct {
+		name      string
+		store     func(ctx sdk.Context)
+		shouldErr bool
+		check     func(ctx sdk.Context)
+	}{
+		{
+			name: "params are migrated properly",
+			store: func(ctx sdk.Context) {
+				// Set the old params
+				paramsSubspace.Set(ctx, types.NicknameParamsKey, types.DefaultNicknameParams())
+				paramsSubspace.Set(ctx, types.DTagParamsKey, types.DefaultDTagParams())
+				paramsSubspace.Set(ctx, types.BioParamsKey, types.DefaultBioParams())
+				paramsSubspace.Set(ctx, types.OracleParamsKey, types.DefaultOracleParams())
+			},
+			check: func(ctx sdk.Context) {
+				// Make sure the params are migrated properly
+				var params types.Params
+				paramsSubspace.GetParamSet(ctx, &params)
+				require.Equal(t, types.DefaultParams(), params)
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := buildContext(keys, tKeys, memKeys)
+			if tc.store != nil {
+				tc.store(ctx)
+			}
+
+			err := v7.MigrateStore(ctx, paramsSubspace)
+			if tc.shouldErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				if tc.check != nil {
+					tc.check(ctx)
+				}
+			}
+		})
+	}
+}
