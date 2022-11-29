e39c58ba0f882b78903d4bf55d40e6be89b3d070
Manuel <manuel.turetta94@gmail.com>
2022-11-28 21:53:44 -0500

    docs: move documentation from github.com/desmos-labs/docs (#1046)
    ## Description
    
    Closes: #XXXX
    
    <!-- Add a description of the changes that this PR introduces and the
    files that
    are the most critical to review. -->
    
    This PR moves the documention from https://github.com/desmos-labs/docs
    into the `docs` folder.
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is
    not applicable and
    please add links to any relevant follow up issues.*
    
    I have...
    
    - [x] included the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] added `!` to the type prefix if API or client breaking change
    - [ ] targeted the correct branch (see [PR
    Targeting](https://github.com/desmos-labs/desmos/blob/master/CONTRIBUTING.md#pr-targeting))
    - [ ] provided a link to the relevant issue or specification
    - [ ] followed the guidelines for [building
    modules](https://docs.cosmos.network/v0.44/building-modules/intro.html)
    - [ ] included the necessary unit and integration
    [tests](https://github.com/desmos-labs/desmos/blob/master/CONTRIBUTING.md#testing)
    - [ ] added a changelog entry to `CHANGELOG.md`
    - [ ] included comments for [documenting Go
    code](https://blog.golang.org/godoc)
    - [ ] updated the relevant documentation or specification
    - [x] reviewed "Files changed" and left comments if necessary
    - [ ] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable
    and please add
    your handle next to the items reviewed if you only reviewed selected
    items.*
    
    I have...
    
    - [ ] confirmed the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] confirmed `!` in the type prefix if API or client breaking change
    - [ ] confirmed all author checklist items have been addressed
    - [ ] reviewed state machine logic
    - [ ] reviewed API design and naming
    - [ ] reviewed documentation is accurate
    - [ ] reviewed tests and test coverage
    - [ ] manually tested (if applicable)

---
+++ docs/docs/05-testnet/04-join-public/01-setup.md
@@ -0,0 +1,28 @@
+---
+id: setup
+title: Setup
+sidebar_label: Setup
+slug: setup
+---
+
+# Setup
+
+:::note Current Testnet  
+See the [testnet repo](https://github.com/desmos-labs/morpheus) for information on the latest testnet, 
+including the correct version of Desmos to use and details about the genesis file.  
+:::
+
+## Validators
+:::warning Requires Desmos executable  
+To join the public testnet you **must** have [`desmos` installed](../../03-fullnode/02-setup.md).  
+:::
+
+To become a testnet validator, the mainnet instructions apply:
+
+1. [Create a full node](../../03-fullnode/02-setup.md).
+2. [Become a validator](../../04-validators/02-setup.md)
+
+The only difference is the Desmos software version and genesis file. See the [testnet repo](https://github.com/desmos-labs/morpheus) for information on testnets, including the correct version of Desmos to use and details about the genesis file.
+
+
+
---
+++ docs/docusaurus.config.js
@@ -0,0 +1,160 @@
+module.exports = {
+  title: 'Desmos documentation',
+  tagline: 'Desmos network official documentation for developers and validators',
+  url: 'https://test-docs.desmos.network',
+  baseUrl: '/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  onDuplicateRoutes: 'warn',
+  favicon: 'assets/favicon.ico',
+  organizationName: 'desmos-labs', // Usually your GitHub org/user name.
+  projectName: 'desmos', // Usually your repo name.
+  themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+      respectPrefersColorScheme: true,
+    },
+    algolia: {
+      apiKey: '492b6729d095b18f5599d6584e00ae11',
+      appId: '1IAGPKAXGP',
+      indexName: 'desmos',
+      contextualSearch: false,
+    },
+    docs: {
+      sidebar: {
+        hideable: true,
+      }
+    },
+    navbar: {
+      logo: {
+        alt: 'Desmos logo',
+        src: 'assets/logo.svg',
+        srcDark: 'assets/logo.svg',
+        href: 'https://docs.desmos.network'
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'intro', // open page of section
+          position: 'left',
+          label: 'Documentation',
+        },
+        // {to: '/blog', label: 'Blog', position: 'left'}, to add extra sections
+        {
+          type: 'docsVersionDropdown',
+          position: 'right',
+          dropdownActiveClassDisabled: true,
+        },
+        /*{
+          // Re-add this if we want to use localization
+          type: 'localeDropdown',
+          position: 'right',
+        },*/
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Related docs',
+          items: [
+            {
+              label: 'Cosmos SDK',
+              href: 'https://docs.cosmos.network',
+            },
+            {
+              label: 'CosmWasm',
+              href: 'https://docs.cosmwasm.com/en/docs/1.0/'
+            }
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/DesmosNetwork',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discord.desmos.network/',
+            },
+            {
+              label: 'Medium',
+              href: 'https://medium.com/desmosnetwork'
+            },
+            {
+              label: 'Telegram',
+              href: 'https://t.me/desmosnetwork',
+            },
+            {
+              label: 'Reddit (not-official)',
+              href: 'https://www.reddit.com/r/DesmosNetwork/'
+            }
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Website',
+              to: 'https://www.desmos.network',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/desmos-labs/desmos',
+            },
+          ],
+        },
+      ],
+      logo: {
+        alt: 'Desmos Logo',
+        src: 'assets/logo.png',
+        href: 'https://www.desmos.network',
+      },
+      copyright: `Copyright © ${new Date().getFullYear()} Desmos Network`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarCollapsible: true,
+          editUrl: 'https://github.com/desmos-labs/desmos/tree/master/docs',
+          showLastUpdateTime: true,
+          lastVersion: "current",
+          versions: {
+            current: {
+              label: "master"
+            },
+          }
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  themes: [
+    '@you54f/theme-github-codeblock'
+  ],
+  plugins: [
+    [
+      "@edno/docusaurus2-graphql-doc-generator",
+      {
+        schema: "docs/07-graphql/schema.graphql",
+        root: "docs/",
+        baseURL: "07-graphql",
+        homepage: "docs/07-graphql/01-overview.md",
+        pretty: true,
+      }
+    ],
+  ]
+  /*i18n: { // add for localization
+    defaultLocale: 'en',
+    locales: ['en', 'chinese'],
+  },*/
+};
