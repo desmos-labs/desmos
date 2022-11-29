d31e985a0fbb64bc1b47fd0d7b343baff79a9177
Manuel Turetta <manuel.turetta94@gmail.com>
2022-11-28 19:06:28 -0500

    docs: completed build script

--- docs/build-all.sh
+++ docs/build-all.sh
@@ -1,46 +1,57 @@
 #!/usr/bin/env bash
 
-function is_legacy_version() {
-  version=$1
-  # Versions wich the documentation is already present inside the "versioned_*" folders
-  legacy_versions=("2.3" "4.2.0")
-
-  for legacy_version in "${legacy_versions[@]}"; do
-    if [ "$legacy_version" == "$version" ] ; then
-      echo "true"
-      return 0
-    fi
-  done
-
-  echo "false"
-  return 0
-}
-
-# This builds the docs.desmos.network docs using docusaurus.
-# Old documentation, which have not been migrated to docusaurus are generated with vuepress.
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+# Current commit
 COMMIT=$(git rev-parse HEAD)
 
-# Create directories where will be stored the old versions
-mkdir -p ~/versioned_docs  ~/versioned_sidebars
+function is_legacy_version() {
+    # Versions with the old documentation schema
+    LEGACY_VERSIONS=("4.2.0" "2.3")
+    version=$1
+    for legacy_version in "${LEGACY_VERSIONS[@]}"; do
+        if [ "$legacy_version" == "$version" ]; then
+            echo "true"
+            return 0
+        fi
+    done
 
-for version in $(jq -r .[] versions.json); do
-  is_legacy=$(is_legacy_version "$version")
-  if [ ! $is_legacy ] ; then
-    echo "not legacy version $version"
-    echo "building docusaurus $version docs"
-    git clean -fdx && git reset --hard && git checkout release/$version.x
-    sh ./pre.sh
-    yarn install && yarn run docusaurus docs:version $version
-    # Archive the built versions
-    mv ./versioned_docs/* ~/versioned_docs/
-    mv ./versioned_sidebars/* ~/versioned_sidebars/
-  fi
+    echo "false"
+    return 0
+}
+
+# Clear old cached docs
+rm -Rf ~/versioned_docs ~/versioned_sidebars
+mkdir -p ~/versioned_docs ~/versioned_sidebars
+
+echo "getting legacy documentation"
+git clone https://github.com/desmos-labs/docs.git "$SCRIPT_DIR/legacy-docs"
+# Cache the legacy documentation
+mv "$SCRIPT_DIR/legacy-docs/versioned_docs/"* ~/versioned_docs/
+mv "$SCRIPT_DIR/legacy-docs/versioned_sidebars/"* ~/versioned_sidebars/
+# Remove the legacy docs files
+rm -Rf "$SCRIPT_DIR/legacy-docs"
+
+for version in $(jq -r .[] "$SCRIPT_DIR/versions.json"); do
+    is_legacy=$(is_legacy_version $version)
+    if ! $is_legacy ; then
+        echo "building docusaurus $version docs"
+        git clean -fdx && git reset --hard && git checkout "v$version"
+        sh ./pre.sh
+        # Remove the versions.json file to ensure that the documentation will be generated properly.
+        # This is needed otherwise the `docusaurus docs:version` command will fail if
+        # you are trying to generate the documentation for a version that is already present in the version.json file.
+        rm "$SCRIPT_DIR/versions.json"
+        yarn install --frozen-lockfile && yarn run docusaurus docs:version $version
+        mv ./versioned_docs/* ~/versioned_docs/
+        mv ./versioned_sidebars/* ~/versioned_sidebars/
+    fi
 done
-
 echo "building docusaurus main docs"
-# git clean -fdx && git reset --hard && git checkout $COMMIT
-mv ~/versioned_docs/* ./versioned_docs/
-mv ~/versioned_sidebars/* ./versioned_sidebars/
+(git clean -fdx && git reset --hard && git checkout $COMMIT)
+mv ~/versioned_docs ~/versioned_sidebars .
 ./pre.sh
-yarn install && yarn build
-# mv build ~/output
+yarn install --frozen-lockfile && yarn build
+
+mv build ~/output
+echo "setup domain"
+echo $DOCS_DOMAIN > ~/output/CNAME
