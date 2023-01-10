42992b91ae9de36a4cb2ece973a65b77f885d548
Paul <p22626262@gmail.com>
2023-01-10 17:37:47 +0800

    chore: remove unused expected keeper

--- x/profiles/testutil/expected_keepers_mocks.go
+++ x/profiles/testutil/expected_keepers_mocks.go
@@ -12,60 +12,8 @@ import (
 	types1 "github.com/cosmos/ibc-go/v3/modules/core/03-connection/types"
 	types2 "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
 	exported "github.com/cosmos/ibc-go/v3/modules/core/exported"
-	types3 "github.com/desmos-labs/desmos/v4/x/subspaces/types"
 	gomock "github.com/golang/mock/gomock"
 )
-
-// MockSubspacesKeeper is a mock of SubspacesKeeper interface.
-type MockSubspacesKeeper struct {
-	ctrl     *gomock.Controller
-	recorder *MockSubspacesKeeperMockRecorder
-}
-
-// MockSubspacesKeeperMockRecorder is the mock recorder for MockSubspacesKeeper.
-type MockSubspacesKeeperMockRecorder struct {
-	mock *MockSubspacesKeeper
-}
-
-// NewMockSubspacesKeeper creates a new mock instance.
-func NewMockSubspacesKeeper(ctrl *gomock.Controller) *MockSubspacesKeeper {
-	mock := &MockSubspacesKeeper{ctrl: ctrl}
-	mock.recorder = &MockSubspacesKeeperMockRecorder{mock}
-	return mock
-}
-
-// EXPECT returns an object that allows the caller to indicate expected use.
-func (m *MockSubspacesKeeper) EXPECT() *MockSubspacesKeeperMockRecorder {
-	return m.recorder
-}
-
-// GetAllSubspaces mocks base method.
-func (m *MockSubspacesKeeper) GetAllSubspaces(ctx types.Context) []types3.Subspace {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "GetAllSubspaces", ctx)
-	ret0, _ := ret[0].([]types3.Subspace)
-	return ret0
-}
-
-// GetAllSubspaces indicates an expected call of GetAllSubspaces.
-func (mr *MockSubspacesKeeperMockRecorder) GetAllSubspaces(ctx interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetAllSubspaces", reflect.TypeOf((*MockSubspacesKeeper)(nil).GetAllSubspaces), ctx)
-}
-
-// HasSubspace mocks base method.
-func (m *MockSubspacesKeeper) HasSubspace(ctx types.Context, subspaceID uint64) bool {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "HasSubspace", ctx, subspaceID)
-	ret0, _ := ret[0].(bool)
-	return ret0
-}
-
-// HasSubspace indicates an expected call of HasSubspace.
-func (mr *MockSubspacesKeeperMockRecorder) HasSubspace(ctx, subspaceID interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "HasSubspace", reflect.TypeOf((*MockSubspacesKeeper)(nil).HasSubspace), ctx, subspaceID)
-}
 
 // MockRelationshipsKeeper is a mock of RelationshipsKeeper interface.
 type MockRelationshipsKeeper struct {
