1cf29f0b1fc992d72a83d7c8bbf21862980d28b7
Riccardo <riccardo.montagnin@gmail.com>
2023-01-24 18:18:28 +0100

    Update x/subspaces/client/cli/query_feegrant.go

--- x/subspaces/client/cli/query_feegrant.go
+++ x/subspaces/client/cli/query_feegrant.go
@@ -38,7 +38,7 @@ func GetAllowancesQueryCmd() *cobra.Command {
 func GetCmdQueryUserAllowances() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "user-allowances [subspace-id] [[grantee]]",
-		Short:   "Query allowances for the given user",
+		Short:   "Query user allowances within a subspace, for an optional given user",
 		Example: fmt.Sprintf(`%s query subspaces user-allowances 1 desmos1evj20rymftvecmgn8t0xv700wkjlgucwfy4f0c`, version.AppName),
 		Args:    cobra.RangeArgs(1, 2),
 		RunE: func(cmd *cobra.Command, args []string) error {
