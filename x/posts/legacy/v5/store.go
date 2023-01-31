131fa9ba6267ef0b195023a991918b4ab404796d
Riccardo <riccardo.montagnin@gmail.com>
2023-01-31 16:19:59 -0500

    feat!: use AttachmentContent instead of Attachment inside poll answers (#1075)
    ## Description
    
    This PR replaces the usage of `Attachment` with `AttachmentContent` inisde `Poll_ProvidedAnswer`. Previously, users were forced to create an `Attachment` object, which also requires the subspace id, post id and a unique id. However, when providing a poll answer, this is not needed since the attachment is stored within the provided answer itself.
    
    We have also introduced a new check to make sure that no poll can be used as another poll's provided answer's attachment: this will make it impossible to have recursive polls. 
    
    #### Note
    Once merged, this PR should be backported into a new `v4.8.x` version to make sure it's shipped inside the testnet and mainnet as well.
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is not applicable and
    please add links to any relevant follow up issues.*
    
    I have...
    
    - [x] included the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [x] added `!` to the type prefix if API or client breaking change
    - [x] targeted the correct branch (see [PR Targeting](https://github.com/desmos-labs/desmos/blob/master/CONTRIBUTING.md#pr-targeting))
    - [ ] provided a link to the relevant issue or specification
    - [x] followed the guidelines for [building modules](https://docs.cosmos.network/v0.44/building-modules/intro.html)
    - [x] included the necessary unit and integration [tests](https://github.com/desmos-labs/desmos/blob/master/CONTRIBUTING.md#testing)
    - [ ] added a changelog entry to `CHANGELOG.md`
    - [x] included comments for [documenting Go code](https://blog.golang.org/godoc)
    - [ ] updated the relevant documentation or specification
    - [x] reviewed "Files changed" and left comments if necessary
    - [ ] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable and please add
    your handle next to the items reviewed if you only reviewed selected items.*
    
    I have...
    
    - [ ] confirmed the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] confirmed `!` in the type prefix if API or client breaking change
    - [ ] confirmed all author checklist items have been addressed
    - [ ] reviewed state machine logic
    - [ ] reviewed API design and naming
    - [ ] reviewed documentation is accurate
    - [ ] reviewed tests and test coverage
    - [ ] manually tested (if applicable)

---
+++ x/posts/legacy/v5/store.go
@@ -0,0 +1,123 @@
+package v5
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	v4 "github.com/desmos-labs/desmos/v4/x/posts/legacy/v4"
+	"github.com/desmos-labs/desmos/v4/x/posts/types"
+)
+
+// MigrateStore performs the migration from version 4 to version 5 of the store.
+// To do this, it iterates over all the post attachments, and converts them to
+// the new storing format (AttachmentContent instead of Attachment).
+// It also removes all the Polls that have been saved as a Poll_ProvidedAnswer's attachment.
+func MigrateStore(ctx sdk.Context, storeKey sdk.StoreKey, cdc codec.BinaryCodec) error {
+	store := ctx.KVStore(storeKey)
+
+	// Migrate the poll attachments
+	return migrateAttachments(store, cdc)
+}
+
+// migrateAttachments migrates all the attachments to v5
+func migrateAttachments(store sdk.KVStore, cdc codec.BinaryCodec) error {
+	attachmentsStore := prefix.NewStore(store, types.AttachmentPrefix)
+	attachmentsIterator := attachmentsStore.Iterator(nil, nil)
+	defer attachmentsIterator.Close()
+
+	for ; attachmentsIterator.Valid(); attachmentsIterator.Next() {
+		// Get the attachment
+		var attachment v4.Attachment
+		err := cdc.Unmarshal(attachmentsIterator.Value(), &attachment)
+		if err != nil {
+			return err
+		}
+
+		var attachmentContent types.AttachmentContent
+		switch content := attachment.Content.GetCachedValue().(type) {
+		case *v4.Media:
+			attachmentContent = migrateMedia(content)
+		case *v4.Poll:
+			v5Poll, err := migratePoll(content, cdc)
+			if err != nil {
+				return err
+			}
+			attachmentContent = v5Poll
+		}
+
+		// Store the new attachment - This will override the old store key
+		v5Attachment := types.NewAttachment(attachment.SubspaceID, attachment.PostID, attachment.ID, attachmentContent)
+		store.Set(
+			types.AttachmentStoreKey(v5Attachment.SubspaceID, v5Attachment.PostID, v5Attachment.ID),
+			cdc.MustMarshal(&v5Attachment),
+		)
+	}
+
+	return nil
+}
+
+// migrateMedia migrates the given media to the new format
+func migrateMedia(media *v4.Media) *types.Media {
+	return types.NewMedia(media.Uri, media.MimeType)
+}
+
+// migratePoll migrates the given poll to the new format
+func migratePoll(poll *v4.Poll, cdc codec.BinaryCodec) (*types.Poll, error) {
+	// Get the new provided answers
+	providedAnswers := make([]types.Poll_ProvidedAnswer, len(poll.ProvidedAnswers))
+	for i, answer := range poll.ProvidedAnswers {
+		attachmentContents, err := migrateProvidedAnswerAttachments(answer.Attachments, cdc)
+		if err != nil {
+			return nil, err
+		}
+		providedAnswers[i] = types.NewProvidedAnswer(answer.Text, attachmentContents)
+	}
+
+	return types.NewPoll(
+		poll.Question,
+		providedAnswers,
+		poll.EndDate,
+		poll.AllowsMultipleAnswers,
+		poll.AllowsAnswerEdits,
+		migratePollFinalTallyResults(poll.FinalTallyResults),
+	), nil
+}
+
+// migrateProvidedAnswerAttachments migrates the given attachments slide
+// converting them into AttachmentContent instances. It also filters all
+// the poll attachments to exclude them
+func migrateProvidedAnswerAttachments(attachments []v4.Attachment, cdc codec.BinaryCodec) ([]types.AttachmentContent, error) {
+	if attachments == nil {
+		return nil, nil
+	}
+
+	var attachmentContents []types.AttachmentContent
+	for _, attachment := range attachments {
+		var content v4.AttachmentContent
+		err := cdc.UnpackAny(attachment.Content, &content)
+		if err != nil {
+			return nil, err
+		}
+
+		// Convert the media only
+		if media, isMedia := content.(*v4.Media); isMedia {
+			attachmentContents = append(attachmentContents, types.NewMedia(media.Uri, media.MimeType))
+		}
+	}
+
+	return attachmentContents, nil
+}
+
+// migratePollFinalTallyResults migrates the given v4 poll tally results into v5
+func migratePollFinalTallyResults(results *v4.PollTallyResults) *types.PollTallyResults {
+	if results == nil {
+		return nil
+	}
+
+	answersResults := make([]types.PollTallyResults_AnswerResult, len(results.Results))
+	for i, result := range results.Results {
+		answersResults[i] = types.NewAnswerResult(result.AnswerIndex, result.Votes)
+	}
+	return types.NewPollTallyResults(answersResults)
+}
---
+++ x/posts/legacy/v5/store_test.go
@@ -0,0 +1,135 @@
+package v5_test
+
+import (
+	"testing"
+	"time"
+
+	v4 "github.com/desmos-labs/desmos/v4/x/posts/legacy/v4"
+	v5 "github.com/desmos-labs/desmos/v4/x/posts/legacy/v5"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
+	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	"github.com/stretchr/testify/require"
+
+	"github.com/desmos-labs/desmos/v4/app"
+	"github.com/desmos-labs/desmos/v4/testutil/storetesting"
+	"github.com/desmos-labs/desmos/v4/x/posts/types"
+	subspacestypes "github.com/desmos-labs/desmos/v4/x/subspaces/types"
+)
+
+func TestMigrateStore(t *testing.T) {
+	cdc, _ := app.MakeCodecs()
+
+	// Build all the necessary keys
+	keys := sdk.NewKVStoreKeys(paramstypes.StoreKey, subspacestypes.StoreKey, types.StoreKey)
+	tKeys := sdk.NewTransientStoreKeys(paramstypes.TStoreKey)
+	memKeys := sdk.NewMemoryStoreKeys(capabilitytypes.MemStoreKey)
+
+	testCases := []struct {
+		name      string
+		store     func(ctx sdk.Context)
+		shouldErr bool
+		check     func(ctx sdk.Context)
+	}{
+		{
+			name: "media attachments are still valid even without migration",
+			store: func(ctx sdk.Context) {
+				media := v4.NewAttachment(1, 1, 1, v4.NewMedia("https://example.com/image.png", "image/png"))
+
+				store := ctx.KVStore(keys[types.StoreKey])
+				store.Set(types.AttachmentStoreKey(1, 1, 1), cdc.MustMarshal(&media))
+			},
+			shouldErr: false,
+			check: func(ctx sdk.Context) {
+				store := ctx.KVStore(keys[types.StoreKey])
+
+				var stored types.Attachment
+				err := cdc.Unmarshal(store.Get(types.AttachmentStoreKey(1, 1, 1)), &stored)
+				require.NoError(t, err)
+				require.Equal(t, types.NewAttachment(1, 1, 1, types.NewMedia(
+					"https://example.com/image.png",
+					"image/png",
+				)), stored)
+			},
+		},
+		{
+			name: "poll provided answers attachments are migrated properly",
+			store: func(ctx sdk.Context) {
+				poll := v4.NewAttachment(1, 1, 1, v4.NewPoll(
+					"What animal is best?",
+					[]v4.Poll_ProvidedAnswer{
+						v4.NewProvidedAnswer("Cat", []v4.Attachment{
+							v4.NewAttachment(1, 1, 1, v4.NewMedia("ftp://user:password@example.com/cat.png", "image/png")),
+						}),
+						v4.NewProvidedAnswer("Dog", []v4.Attachment{
+							v4.NewAttachment(1, 1, 1, v4.NewPoll(
+								"Is this a nested poll?",
+								[]v4.Poll_ProvidedAnswer{
+									v4.NewProvidedAnswer("Yes", nil),
+									v4.NewProvidedAnswer("No", nil),
+								},
+								time.Date(2020, time.January, 1, 0, 0, 0, 0, time.UTC),
+								false,
+								false,
+								nil,
+							)),
+						}),
+					},
+					time.Date(2020, 1, 1, 12, 00, 00, 000, time.UTC),
+					false,
+					false,
+					v4.NewPollTallyResults([]v4.PollTallyResults_AnswerResult{
+						v4.NewAnswerResult(0, 1),
+					}),
+				))
+
+				store := ctx.KVStore(keys[types.StoreKey])
+				store.Set(types.AttachmentStoreKey(1, 1, 1), cdc.MustMarshal(&poll))
+			},
+			shouldErr: false,
+			check: func(ctx sdk.Context) {
+				store := ctx.KVStore(keys[types.StoreKey])
+
+				var stored types.Attachment
+				err := cdc.Unmarshal(store.Get(types.AttachmentStoreKey(1, 1, 1)), &stored)
+				require.NoError(t, err)
+				require.Equal(t, types.NewAttachment(1, 1, 1, types.NewPoll(
+					"What animal is best?",
+					[]types.Poll_ProvidedAnswer{
+						types.NewProvidedAnswer("Cat", []types.AttachmentContent{
+							types.NewMedia("ftp://user:password@example.com/cat.png", "image/png"),
+						}),
+						types.NewProvidedAnswer("Dog", nil),
+					},
+					time.Date(2020, 1, 1, 12, 00, 00, 000, time.UTC),
+					false,
+					false,
+					types.NewPollTallyResults([]types.PollTallyResults_AnswerResult{
+						types.NewAnswerResult(0, 1),
+					}),
+				)), stored)
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := storetesting.BuildContext(keys, tKeys, memKeys)
+			if tc.store != nil {
+				tc.store(ctx)
+			}
+
+			err := v5.MigrateStore(ctx, keys[types.StoreKey], cdc)
+			if tc.shouldErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				if tc.check != nil {
+					tc.check(ctx)
+				}
+			}
+		})
+	}
+}
