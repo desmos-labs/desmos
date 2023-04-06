6f30b90d45282b9a13aeaa8fefb7862f4dd8857c
Paul <p22626262@gmail.com>
2023-04-06 20:17:58 +0800

    docs: add post handler comment

--- app/post_handler.go
+++ app/post_handler.go
@@ -5,6 +5,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/auth/posthandler"
 )
 
+// NewPostHandler returns an empty PostHandler chain.
+// PostHandler is like AnteHandler but it executes after RunMsgs.
 func NewPostHandler() (sdk.PostHandler, error) {
 	return posthandler.NewPostHandler(
 		posthandler.HandlerOptions{},
