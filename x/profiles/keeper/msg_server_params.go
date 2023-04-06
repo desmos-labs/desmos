f642c0b42bceeafa282c8a7815da9724af7b98fb
Paul <p22626262@gmail.com>
2023-04-06 17:51:38 +0800

    chore: move msg server params handler from msg server profiles

---
+++ x/profiles/keeper/msg_server_params.go
@@ -0,0 +1,32 @@
+package keeper
+
+import (
+	"context"
+
+	errors "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	"github.com/desmos-labs/desmos/v4/x/profiles/types"
+)
+
+// UpdateParams updates the module parameters
+func (m msgServer) UpdateParams(goCtx context.Context, msg *types.MsgUpdateParams) (*types.MsgUpdateParamsResponse, error) {
+	authority := m.authority
+	if authority != msg.Authority {
+		return nil, errors.Wrapf(govtypes.ErrInvalidSigner, "invalid authority; expected %s, got %s", authority, msg.Authority)
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	m.SetParams(ctx, msg.Params)
+
+	ctx.EventManager().EmitEvents(sdk.Events{
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
+			sdk.NewAttribute(sdk.AttributeKeyAction, sdk.MsgTypeURL(msg)),
+			sdk.NewAttribute(sdk.AttributeKeySender, msg.Authority),
+		),
+	})
+
+	return &types.MsgUpdateParamsResponse{}, nil
+}
