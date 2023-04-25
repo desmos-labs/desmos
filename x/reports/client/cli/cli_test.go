0a07b4963c3317ad690f170f2f69c292a12e8501
dadamu <p22626262@gmail.com>
2023-04-25 19:54:11 +0800

    fix: fix posts reference cli tests

--- x/reports/client/cli/cli_test.go
+++ x/reports/client/cli/cli_test.go
@@ -86,6 +86,7 @@ func (s *IntegrationTestSuite) SetupSuite() {
 				poststypes.REPLY_SETTING_EVERYONE,
 				time.Date(2020, 1, 1, 12, 00, 00, 000, time.UTC),
 				nil,
+				"cosmos13t6y2nnugtshwuy0zkrq287a95lyy8vzleaxmd",
 			),
 		},
 		[]poststypes.PostDataEntry{
