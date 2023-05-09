c89e8efce1d527681df5b09dee7733489e9eddc0
Paul <p22626262@gmail.com>
2023-05-09 19:10:43 +0800

    revert: posts migration keeper pointer

--- x/posts/module.go
+++ x/posts/module.go
@@ -128,7 +128,7 @@ func (am AppModule) RegisterServices(cfg module.Configurator) {
 	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(*am.keeper))
 	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
 
-	m := keeper.NewMigrator(am.keeper, am.sk, am.legacySubspace)
+	m := keeper.NewMigrator(*am.keeper, am.sk, am.legacySubspace)
 	err := cfg.RegisterMigration(types.ModuleName, 1, m.Migrate1to2)
 	if err != nil {
 		panic(err)
