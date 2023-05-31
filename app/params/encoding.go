d2b7aa6b915e7284a9cfbcd8fd60f7025e55fc35
Riccardo <riccardo.montagnin@gmail.com>
2023-05-31 12:24:40 -0500

    refactor: use simappparams.EncodingConfig instead of custom EncodingConfig

--- app/params/encoding.go
+++ app/params/encoding.go
@@ -1,31 +1,22 @@
 package params
 
 import (
-	"github.com/cosmos/cosmos-sdk/client"
+	simappparams "cosmossdk.io/simapp/params"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/x/auth/tx"
 )
 
-// EncodingConfig specifies the concrete encoding types to use for a given app.
-// This is provided for compatibility between protobuf and amino implementations.
-type EncodingConfig struct {
-	InterfaceRegistry types.InterfaceRegistry
-	Marshaler         codec.Codec
-	TxConfig          client.TxConfig
-	Amino             *codec.LegacyAmino
-}
-
 // MakeEncodingConfig creates an EncodingConfig for an amino based test configuration.
-func MakeEncodingConfig() EncodingConfig {
+func MakeEncodingConfig() simappparams.EncodingConfig {
 	amino := codec.NewLegacyAmino()
 	interfaceRegistry := types.NewInterfaceRegistry()
-	marshaler := codec.NewProtoCodec(interfaceRegistry)
-	txCfg := tx.NewTxConfig(marshaler, tx.DefaultSignModes)
+	cdc := codec.NewProtoCodec(interfaceRegistry)
+	txCfg := tx.NewTxConfig(cdc, tx.DefaultSignModes)
 
-	return EncodingConfig{
+	return simappparams.EncodingConfig{
 		InterfaceRegistry: interfaceRegistry,
-		Marshaler:         marshaler,
+		Codec:             cdc,
 		TxConfig:          txCfg,
 		Amino:             amino,
 	}
