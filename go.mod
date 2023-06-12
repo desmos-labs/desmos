609198c805ab5b55425eb75f3c0854218d9a48fe
Paul Chen <p22626262@gmail.com>
2023-06-12 18:16:11 +0800

    build: use osmosis v15.1.2-desmos version

--- go.mod
+++ go.mod
@@ -335,7 +335,8 @@ replace (
 	// TODO Remove it: https://github.com/cosmos/cosmos-sdk/issues/10409
 	github.com/gin-gonic/gin => github.com/gin-gonic/gin v1.8.1
 
-	github.com/osmosis-labs/osmosis/v15 => github.com/desmos-labs/osmosis/v15 v15.0.0-20230612085123-7cf9d03abd56
+	// Our cosmos-sdk branch is: https://github.com/desmos-labs/cosmos-sdk v0.47.x-desmos
+	github.com/osmosis-labs/osmosis/v15 => github.com/desmos-labs/osmosis/v15 v15.1.2-desmos
 
 	// Downgraded to avoid bugs in following commits which caused simulations to fail.
 	github.com/syndtr/goleveldb => github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7
--- x/tokenfactory/types/params.go
+++ x/tokenfactory/types/params.go
@@ -8,31 +8,27 @@ import (
 // NewParams creates a new params instance
 func NewParams(denomCreationFee sdk.Coins, denomCreationGasConsume uint64) Params {
 	return Params{
-		DenomCreationFee:        denomCreationFee,
-		DenomCreationGasConsume: denomCreationGasConsume,
+		DenomCreationFee: denomCreationFee,
 	}
 }
 
 // DefaultParams creates a default params instance
 func DefaultParams() Params {
 	return Params{
-		DenomCreationFee:        sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 10_000_000)), // 10 DSM
-		DenomCreationGasConsume: 0,
+		DenomCreationFee: sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 10_000_000)), // 10 DSM
 	}
 }
 
 // ToOsmosisTokenFactoryParams converts desmos tokenfactory Params into osmosis tokenfactory Params
 func ToOsmosisTokenFactoryParams(p Params) tokenfactorytypes.Params {
 	return tokenfactorytypes.Params{
-		DenomCreationFee:        p.DenomCreationFee,
-		DenomCreationGasConsume: p.DenomCreationGasConsume,
+		DenomCreationFee: p.DenomCreationFee,
 	}
 }
 
 // FromOsmosisTokenFactoryParams converts osmosis tokenfactory Params into desmos tokenfactory Params
 func FromOsmosisTokenFactoryParams(p tokenfactorytypes.Params) Params {
 	return Params{
-		DenomCreationFee:        p.DenomCreationFee,
-		DenomCreationGasConsume: p.DenomCreationGasConsume,
+		DenomCreationFee: p.DenomCreationFee,
 	}
 }
