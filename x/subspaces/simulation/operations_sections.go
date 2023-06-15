0638c677968547e7c5ea7fbc432d9eb2db956a8c
Paul Chen <p22626262@gmail.com>
2023-06-15 22:35:14 +0800

    fix(tests): fix move section simulation test (#1174)
    ## Description
    
    Closes: #XXXX
    This PR fixes the move section simulation test that causes the invalid
    section path.
    
    <!-- Add a description of the changes that this PR introduces and the
    files that
    are the most critical to review. -->
    
    ---
    
    ### Author Checklist
    
    *All items are required. Please add a note to the item if the item is
    not applicable and
    please add links to any relevant follow up issues.*
    
    I have...
    
    - [x] included the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] added `!` to the type prefix if API or client breaking change
    - [x] targeted the correct branch (see [PR
    Targeting](https://github.com/desmos-labs/desmos/blob/master/CONTRIBUTING.md#pr-targeting))
    - [ ] provided a link to the relevant issue or specification
    - [ ] followed the guidelines for [building
    modules](https://docs.cosmos.network/v0.44/building-modules/intro.html)
    - [ ] included the necessary unit and integration
    [tests](https://github.com/desmos-labs/desmos/blob/master/CONTRIBUTING.md#testing)
    - [ ] added a changelog entry to `CHANGELOG.md`
    - [ ] included comments for [documenting Go
    code](https://blog.golang.org/godoc)
    - [ ] updated the relevant documentation or specification
    - [x] reviewed "Files changed" and left comments if necessary
    - [ ] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    *All items are required. Please add a note if the item is not applicable
    and please add
    your handle next to the items reviewed if you only reviewed selected
    items.*
    
    I have...
    
    - [ ] confirmed the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] confirmed `!` in the type prefix if API or client breaking change
    - [ ] confirmed all author checklist items have been addressed
    - [ ] reviewed state machine logic
    - [ ] reviewed API design and naming
    - [ ] reviewed documentation is accurate
    - [ ] reviewed tests and test coverage
    - [ ] manually tested (if applicable)
    
    ---------
    
    Co-authored-by: Riccardo Montagnin <riccardo.montagnin@gmail.com>

--- x/subspaces/simulation/operations_sections.go
+++ x/subspaces/simulation/operations_sections.go
@@ -209,16 +209,14 @@ func randomMoveSectionFields(
 
 	// Get a random new parent
 	parent := RandomSection(r, sections)
-	newParentID = parent.ID
-	if newParentID == sectionID {
+	if parent.ID == sectionID {
 		// Skip because we can't move the section to itself
 		skip = true
 		return
 	}
 
-	section.ParentID = newParentID
-	if parent.ParentID == sectionID {
-		// Skip because the new section path is invalid
+	if isChildSection(ctx, k, section, parent) {
+		// Skip because we can't move the section to its child section
 		skip = true
 		return
 	}
@@ -234,6 +232,19 @@ func randomMoveSectionFields(
 	account = *acc
 
 	return subspaceID, sectionID, newParentID, account, false
+}
+
+// isChildSection tells whether the given section is the child of the given parent section or not.
+func isChildSection(ctx sdk.Context, k keeper.Keeper, parent types.Section, section types.Section) bool {
+	visitsCount := 0
+	k.IterateSectionChildren(ctx, parent.SubspaceID, parent.ID, func(node types.Section) (stop bool) {
+		if node.SubspaceID == section.SubspaceID && node.ID == section.ID {
+			visitsCount++
+		}
+		return visitsCount > 1
+	})
+
+	return visitsCount == 1
 }
 
 // --------------------------------------------------------------------------------------------------------------------
