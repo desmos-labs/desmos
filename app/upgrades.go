61393a096e34e8a345c8a09e8706bb4384501378
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-06-23 09:40:56 -0500

    feat: add v5.2.0 upgrade handler

--- app/upgrades.go
+++ app/upgrades.go
@@ -2,9 +2,11 @@ package app
 
 import (
 	v500 "github.com/desmos-labs/desmos/v5/app/upgrades/v500"
+	v520 "github.com/desmos-labs/desmos/v5/app/upgrades/v520"
 )
 
 // registerUpgradeHandlers registers all the upgrade handlers that are supported by the app
 func (app *DesmosApp) registerUpgradeHandlers() {
 	app.registerUpgrade(v500.NewUpgrade(app.ModuleManager, app.Configurator(), app.ParamsKeeper, app.ConsensusParamsKeeper))
+	app.registerUpgrade(v520.NewUpgrade(app.ModuleManager, app.Configurator(), app.ParamsKeeper, app.ConsensusParamsKeeper))
 }
