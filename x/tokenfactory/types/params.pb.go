6500577da0d3746b4af32a13d092dc6a99c841be
Paul Chen <p22626262@gmail.com>
2023-06-28 21:25:26 +0800

    chore: apply suggestions

--- x/tokenfactory/types/params.pb.go
+++ x/tokenfactory/types/params.pb.go
@@ -31,7 +31,7 @@ const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 type Params struct {
 	// DenomCreationFee defines the fee to be charged on the creation of a new
 	// denom. The fee is drawn from the subspace treasruy account, and
-	// transferred to the community pool.
+	// burned.
 	DenomCreationFee github_com_cosmos_cosmos_sdk_types.Coins `protobuf:"bytes,1,rep,name=denom_creation_fee,json=denomCreationFee,proto3,castrepeated=github.com/cosmos/cosmos-sdk/types.Coins" json:"denom_creation_fee" yaml:"denom_creation_fee"`
 }
 
