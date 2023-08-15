c54bca45415c00096adec5160dfff4278393b285
Paul Chen <p22626262@gmail.com>
2023-08-15 15:17:04 +0800

    chore: fix lint

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 cd proto
-proto_dirs=$(find ./desmos -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
     if grep "option go_package" $file &> /dev/null ; then
