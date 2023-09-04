6ef4e0d1693ec2788d453948c327feaf0576f5d8
Paul Chen <p22626262@gmail.com>
2023-09-04 20:26:49 +0800

    test: add profile mock hooks

--- scripts/mockgen.sh
+++ scripts/mockgen.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
 mockgen_cmd="mockgen"
-search_path="/types/expected_keepers.go"
 
-find ./x -path "*$search_path" | while read -r source_file; do
-    destination_file="${source_file/types\/expected_keepers.go/testutil\/expected_keepers_mocks.go}"
-    destination_dir=$(dirname "$destination_file")
-    package=$(basename "$destination_dir")
+# Define an array of source and destination paths
+declare -a paths=(
+    "types/expected_keepers.go:testutil/expected_keepers_mocks.go"
+    "types/hooks.go:testutil/hooks_mocks.go"
+)
 
-    mkdir -p "$destination_dir"
-    $mockgen_cmd -source "$source_file" -package "$package" -destination "$destination_file"
+# Loop through the paths array and generate mocks for each source file
+for path_pair in "${paths[@]}"; do
+    source_path=$(echo "$path_pair" | cut -d ':' -f 1)
+    destination_path=$(echo "$path_pair" | cut -d ':' -f 2)
+
+    find ./x -path "*$source_path" | while read -r source_file; do
+        destination_file="${source_file/$source_path/$destination_path}"
+        destination_dir=$(dirname "$destination_file")
+        package=$(basename "$destination_dir")
+
+        mkdir -p "$destination_dir"
+        $mockgen_cmd -source "$source_file" -package "$package" -destination "$destination_file"
+    done
 done
