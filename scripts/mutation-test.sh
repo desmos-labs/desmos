a678f13195705deb6b8adccb65303b0961e2dc10
Paul Chen <p22626262@gmail.com>
2023-09-06 19:42:15 +0800

    ci: improve mutation script

--- scripts/mutation-test.sh
+++ scripts/mutation-test.sh
@@ -5,14 +5,14 @@ oIFS="$IFS"; IFS=, ; set -- $1 ; IFS="$oIFS"
 DISABLED_MUTATORS='branch/*'
 
 # Only consider the following:
-# * go files in types, keeper, or module root directories
+# * go files in types, keeper directories
 # * ignore test and Protobuf files
 # * ignore simulation test files
 # * ignore legacy files
 # * ignore legacy wasm files
 # * ignore testutil files
-# * ignore codec.go and invariants.go
-go_file_exclusions="-type f -path */keeper/* -or -path */types/* -name *.go -and -not -path */legacy/* -and -not -name *_test.go -and -not -name *pb* -and -not -name module.go -and -not -name ibc_module.go -and -not -name codec.go -and -not -name invariants.go"
+# * ignore codec.go
+go_file_exclusions="-type f -path */keeper/* -or -path */types/* -name *.go -and -not -path */legacy/* -and -not -name *_test.go -and -not -name *pb* -and -not -name codec.go"
 MUTATION_SOURCES=$(find ./x $go_file_exclusions )
 
 # Filter on a module-by-module basis as provided by input
