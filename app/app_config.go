4f36d5f82e3660a6b5fc81cb876d5eb221d209b1
Paul Chen <p22626262@gmail.com>
2023-09-12 03:18:37 +0800

    fix: disable buggy wasm metrics

--- app/app_config.go
+++ app/app_config.go
@@ -18,10 +18,7 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/x/consensus"
 
-	"github.com/prometheus/client_golang/prometheus"
-
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
-	"github.com/spf13/cast"
 
 	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
 
@@ -125,9 +122,12 @@ func GetWasmOpts(
 	reactionsKeeper reactionskeeper.Keeper,
 ) []wasmkeeper.Option {
 	var wasmOpts []wasmkeeper.Option
-	if cast.ToBool(appOpts.Get("telemetry.enabled")) {
-		wasmOpts = append(wasmOpts, wasmkeeper.WithVMCacheMetrics(prometheus.DefaultRegisterer))
-	}
+	// FIXME (wasmd-1575): This is commented out temporarily because it causes panics in telemetry server due to buggy
+	// Bug has been fixed here: https://github.com/CosmWasm/wasmd/pull/1575
+	// and will be released in v0.42
+	// if cast.ToBool(appOpts.Get("telemetry.enabled")) {
+	// wasmOpts = append(wasmOpts, wasmkeeper.WithVMCacheMetrics(prometheus.DefaultRegisterer))
+	// }
 
 	customQueryPlugin := NewDesmosCustomQueryPlugin(
 		cdc,
