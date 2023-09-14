9be2113fdb8a3a9912ae8bb508baf3b9d3cb2da9
Alessandro Mazzon <alessandro@forbole.com>
2023-09-14 14:18:53 +0200

    feat: added tailwind

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -177,6 +177,17 @@ module.exports = {
         pretty: true,
       },
     ],
+    async function myPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
   ],
   /*i18n: { // add for localization
     defaultLocale: 'en',
---
+++ docs/tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      md: "768px",
+      lg: "1280px",
+      xl: "1920px",
+    },
+    extend: {
+      width: {
+        mobile: "375px",
+        md: "768px",
+        lg: "1280px",
+        xl: "1920px",
+      },
+      padding: {
+        xMobile: "20px",
+        xMd: "40px",
+        xLg: "90px",
+        xXl: "100px",
+        yMobile: "26px",
+        yMd: "60px",
+        yLg: "80px",
+        yXl: "80px",
+        "navbar-mobile": "60px",
+        "navbar-md": "60px",
+      },
+      margin: {
+        xMobile: "20px",
+        xMd: "40px",
+        xLg: "90px",
+        xXl: "100px",
+        yMobile: "26px",
+        yMd: "60px",
+        yLg: "80px",
+        yXl: "80px",
+        "navbar-mobile": "60px",
+        "navbar-md": "60px",
+      },
+    },
+  },
+  plugins: [],
+};
