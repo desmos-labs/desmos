9de9a7050c818265f54c60b344b1b8b45be7eb93
Paul Chen <p22626262@gmail.com>
2023-09-22 18:43:18 +0800

    chore: remove unused upgrades

--- app/upgrades.go
+++ app/upgrades.go
@@ -1,14 +1,6 @@
 package app
 
-import (
-	v620 "github.com/desmos-labs/desmos/v6/app/upgrades/v620"
-	v630 "github.com/desmos-labs/desmos/v6/app/upgrades/v630"
-	v640 "github.com/desmos-labs/desmos/v6/app/upgrades/v640"
-)
-
 // registerUpgradeHandlers registers all the upgrade handlers that are supported by the app
 func (app *DesmosApp) registerUpgradeHandlers() {
-	app.registerUpgrade(v620.NewUpgrade(app.ModuleManager, app.Configurator()))
-	app.registerUpgrade(v630.NewUpgrade(app.ModuleManager, app.Configurator()))
-	app.registerUpgrade(v640.NewUpgrade(app.ModuleManager, app.Configurator()))
+
 }
