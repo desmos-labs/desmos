a2e68ac22382e17dccb99b2e435ccf7586a7969e
Paul Chen <p22626262@gmail.com>
2023-09-28 02:05:56 +0800

    chore: update make encoding config

--- app/params/encoding.go
+++ app/params/encoding.go
@@ -2,15 +2,28 @@ package params
 
 import (
 	simappparams "cosmossdk.io/simapp/params"
+	"cosmossdk.io/x/tx/signing"
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/address"
 	"github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth/tx"
+	"github.com/cosmos/gogoproto/proto"
 )
 
 // MakeEncodingConfig creates an EncodingConfig for an amino based test configuration.
 func MakeEncodingConfig() simappparams.EncodingConfig {
 	amino := codec.NewLegacyAmino()
-	interfaceRegistry := types.NewInterfaceRegistry()
+	interfaceRegistry, err := types.NewInterfaceRegistryWithOptions(types.InterfaceRegistryOptions{
+		ProtoFiles: proto.HybridResolver,
+		SigningOptions: signing.Options{
+			AddressCodec:          address.NewBech32Codec(sdk.Bech32MainPrefix),
+			ValidatorAddressCodec: address.NewBech32Codec(sdk.Bech32MainPrefix + sdk.PrefixValidator + sdk.PrefixOperator),
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
 	marshaler := codec.NewProtoCodec(interfaceRegistry)
 	txCfg := tx.NewTxConfig(marshaler, tx.DefaultSignModes)
 
