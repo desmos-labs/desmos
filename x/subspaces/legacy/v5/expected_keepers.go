d40c2b88b27ba23b92c127173ad6448cdb173f64
Paul Chen <p22626262@gmail.com>
2023-09-27 15:12:41 +0800

    chore: update subspaces legacy simd expected keepers

--- x/subspaces/legacy/v5/expected_keepers.go
+++ x/subspaces/legacy/v5/expected_keepers.go
@@ -1,13 +1,15 @@
 package v5
 
 import (
+	"context"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // DONTCOVER
 
 type AccountKeeper interface {
-	NewAccountWithAddress(ctx sdk.Context, addr sdk.AccAddress) sdk.AccountI
-	HasAccount(ctx sdk.Context, addr sdk.AccAddress) bool
-	SetAccount(ctx sdk.Context, acc sdk.AccountI)
+	NewAccountWithAddress(ctx context.Context, addr sdk.AccAddress) sdk.AccountI
+	HasAccount(ctx context.Context, addr sdk.AccAddress) bool
+	SetAccount(ctx context.Context, acc sdk.AccountI)
 }
