9f2500b5cda10cb0efb1f7fcd6756bf0a6913583
Paul Chen <p22626262@gmail.com>
2023-09-28 00:53:03 +0800

    test: add finalize block state in sim app export test

--- app/app_test.go
+++ app/app_test.go
@@ -86,11 +86,18 @@ func TestSimAppExportAndBlockedAddrs(t *testing.T) {
 		)
 	}
 
-	app.Commit()
+	// finalize block so we have CheckTx state set
+	_, err = app.FinalizeBlock(&abci.RequestFinalizeBlock{
+		Height: 1,
+	})
+	require.NoError(t, err)
+
+	_, err = app.Commit()
+	require.NoError(t, err)
 
 	// Making a new app object with the db, so that initchain hasn't been called
 	app2 := NewDesmosApp(
-		log.NewTestLogger(t), db, nil, true, simtestutil.NewAppOptionsWithFlagHome(t.TempDir()),
+		logger.With("instance", "second"), db, nil, true, simtestutil.NewAppOptionsWithFlagHome(t.TempDir()),
 	)
 	_, err = app2.ExportAppStateAndValidators(false, []string{}, []string{})
 	require.NoError(t, err, "ExportAppStateAndValidators should not have an error")
