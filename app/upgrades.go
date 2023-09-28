bda1b19793c112299d8a8e460a37d4101317b9c9
Paul Chen <p22626262@gmail.com>
2023-09-28 19:50:28 +0800

    chore: add v7 upgrade

--- app/upgrades.go
+++ app/upgrades.go
@@ -1,6 +1,10 @@
 package app
 
+import (
+	v7 "github.com/desmos-labs/desmos/v6/app/upgrades/v7"
+)
+
 // registerUpgradeHandlers registers all the upgrade handlers that are supported by the app
 func (app *DesmosApp) registerUpgradeHandlers() {
-
+	app.registerUpgrade(v7.NewUpgrade(app.ModuleManager, app.Configurator()))
 }
