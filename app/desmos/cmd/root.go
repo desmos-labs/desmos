be9ba4a30e3c36dc7ce7569272c9913b038fc1ca
Paul Chen <p22626262@gmail.com>
2023-11-02 18:25:37 +0800

    chore: fix lint

--- app/desmos/cmd/root.go
+++ app/desmos/cmd/root.go
@@ -76,7 +76,8 @@ func NewRootCmd() *cobra.Command {
 
 			// This needs to go after ReadFromClientConfig, as that function
 			// sets the RPC client needed for SIGN_MODE_TEXTUAL.
-			enabledSignModes := append(tx.DefaultSignModes, signing.SignMode_SIGN_MODE_TEXTUAL)
+			enabledSignModes := tx.DefaultSignModes
+			enabledSignModes = append(enabledSignModes, signing.SignMode_SIGN_MODE_TEXTUAL)
 			txConfigOpts := tx.ConfigOptions{
 				EnabledSignModes:           enabledSignModes,
 				TextualCoinMetadataQueryFn: txmodule.NewGRPCCoinMetadataQueryFn(initClientCtx),
