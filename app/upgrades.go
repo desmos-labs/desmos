8c78739127f736cd45f38df43e5a343785d86968
Paul Chen <p22626262@gmail.com>
2024-08-08 18:37:51 +0800

    chore: prepare v7.2.0

--- app/upgrades.go
+++ app/upgrades.go
@@ -3,10 +3,12 @@ package app
 import (
 	v700 "github.com/desmos-labs/desmos/v7/app/upgrades/v700"
 	v710 "github.com/desmos-labs/desmos/v7/app/upgrades/v710"
+	v720 "github.com/desmos-labs/desmos/v7/app/upgrades/v720"
 )
 
 // registerUpgradeHandlers registers all the upgrade handlers that are supported by the app
 func (app *DesmosApp) registerUpgradeHandlers() {
 	app.registerUpgrade(v700.NewUpgrade(app.ModuleManager, app.Configurator()))
 	app.registerUpgrade(v710.NewUpgrade(app.ModuleManager, app.Configurator()))
+	app.registerUpgrade(v720.NewUpgrade(app.ModuleManager, app.Configurator()))
 }
